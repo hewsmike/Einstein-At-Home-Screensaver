f9bd08385aa2bb447a5c940648460e3de8569cf1
Mike Hewson <hewsmike@iinet.net.au>
2011-10-15 21:05:48 +1100

    Craft class
    Models a manoueverable spacecraft

---
+++ src/solarsystem/Craft.cpp
@@ -0,0 +1,153 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Craft.h"
+
+const vec_t Craft::MAX_RANGE(Universe::CELESTIAL_SPHERE_RADIUS * 8);
+const vec_t Craft::MIN_EARTH_RANGE(Earth::EARTH_RADIUS*(1.2f));
+const vec_t Craft::START_RADIUS(Earth::EARTH_RADIUS*(2.0f));
+const vec_t Craft::RETURN_SPEED(5.0f);
+const vec_t Craft::MAX_SPEED(50.0f);
+const vec_t Craft::PITCH_RATE((PI*2.0f)/180.0f);
+const vec_t Craft::ROLL_RATE((PI*2.0f)/180.0f);
+const vec_t Craft::SPEED_DEC(2.5f);
+const vec_t Craft::SPEED_INC(2.5f);
+const vec_t Craft::LATERAL_THRUST_RATE(0.5f);
+const vec_t Craft::VERTICAL_THRUST_RATE(0.5f);
+const vec_t Craft::YAW_RATE((PI*2.0f)/180.0f);
+
+Craft::Craft() {
+   go_home();
+  	}
+
+Craft::~Craft() {
+	}
+
+const InertialPlatform& Craft::get_platform() const {
+	return state;
+	}
+
+void Craft::step(void) {
+   state.translate();
+   // Bounds and collision checking ....
+
+   // Too far away from home.
+   if(state.position().len() > Craft::MAX_RANGE) {
+      // TODO turn him around to point homewards?
+      // Give a little nudge to send it back in the direction of home.
+      state.set_velocity(-Craft::RETURN_SPEED*state.position().unit());
+      std::cout << "Too far away from Earth - nudged back" << std::endl;
+      }
+
+   // Too close to home
+   if(state.position().len() < Craft::MIN_EARTH_RANGE) {
+      // TODO turn him around to point outwards?
+      // Give a little nudge to send it back away from home.
+      state.set_velocity(+Craft::RETURN_SPEED*state.position().unit());
+      std::cout << "Too close to Earth - nudged away" << std::endl;
+      }
+
+   // TODO - to close to the Sun
+   }
+
+void Craft::forward_thrust(void) {
+   // Add to the current velocity vector a fraction of the 'look' vector.
+   // That is : thrust is applied along the aft to fore axis.
+   vector_thrust(+Craft::SPEED_INC*state.look());
+   }
+
+void Craft::go_home(void) {
+   // Ought place him initially in good position, stationary.
+   // TODO looking at Earth ?
+   state.set_position(Vector3D(Craft::START_RADIUS, 0, 3*Craft::START_RADIUS));
+   stop();
+   }
+
+void Craft::reverse_thrust(void) {
+   // Subtract from the current velocity vector a fraction of the 'look' vector.
+   // That is : thrust is applied along the fore to aft axis.
+   vector_thrust(-Craft::SPEED_DEC*state.look());
+   }
+
+void Craft::nose_down() {
+   state.pitch_down(Craft::PITCH_RATE);
+   }
+
+void Craft::nose_up() {
+   state.pitch_up(Craft::PITCH_RATE);
+   }
+
+void Craft::roll_right() {
+   state.roll_right(Craft::ROLL_RATE);
+   }
+
+void Craft::roll_left() {
+   state.roll_left(Craft::ROLL_RATE);
+   }
+
+void Craft::stop(void) {
+   state.set_velocity(Vector3D(0.0f, 0.0f, 0.0f));
+   }
+
+void Craft::right_thrust() {
+   // Add to the current velocity vector a fraction of the 'cross' vector.
+   // That is : thrust is applied along the left to right wing axis.
+   vector_thrust(+Craft::LATERAL_THRUST_RATE*state.cross());
+   }
+
+void Craft::left_thrust() {
+   // Add to the current velocity vector a fraction of the 'cross' vector.
+   // That is : thrust is applied along the right to left wing axis.
+   vector_thrust(-Craft::LATERAL_THRUST_RATE*state.cross());
+   }
+
+void Craft::up_thrust() {
+   // Add to the current velocity vector a fraction of the 'up' vector.
+   // That is : thrust is applied along the floor to ceiling axis.
+   vector_thrust(+Craft::VERTICAL_THRUST_RATE*state.up());
+   }
+
+void Craft::down_thrust() {
+   // Subtract from the current velocity vector a fraction of the 'up' vector.
+   // That is : thrust is applied along the ceiling to floor axis.
+   vector_thrust(-Craft::VERTICAL_THRUST_RATE*state.up());
+   }
+
+void Craft::yaw_right() {
+   state.yaw_right(Craft::YAW_RATE);
+   }
+
+void Craft::yaw_left() {
+   state.yaw_left(Craft::YAW_RATE);
+   }
+
+void Craft::vector_thrust(Vector3D thrust) {
+   // Apply thrust along given vector.
+   Vector3D new_vel = state.velocity() + thrust;
+
+    // How fast are we going? Too fast?
+   if(new_vel.len() > Craft::MAX_SPEED) {
+      // Yes, so cap speed along the line of the new velocity.
+      new_vel = Craft::MAX_SPEED*new_vel.unit();
+      std::cout << "Speed capped at " << new_vel.len() << std::endl;
+      }
+
+   state.set_velocity(new_vel);
+   }
---
+++ src/solarsystem/Craft.h
@@ -0,0 +1,87 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef CRAFT_H_
+#define CRAFT_H_
+
+#include <iostream>
+
+#include "Earth.h"
+#include "SolarSystemGlobals.h"
+#include "InertialPlatform.h"
+#include "Universe.h"
+
+class Craft{
+	private:
+      /// Global bounds for craft position
+      static const vec_t MAX_RANGE;
+      static const vec_t MIN_EARTH_RANGE;
+
+      static const vec_t START_RADIUS;
+
+      /// Top speed of craft in whatever nett direction
+      static const vec_t MAX_SPEED;
+
+      /// Rates for about centre of mass craft rotations
+      static const vec_t PITCH_RATE;
+      static const vec_t ROLL_RATE;
+      static const vec_t YAW_RATE;
+
+      /// Speed setting post 'bounce' off object
+      static const vec_t RETURN_SPEED;
+
+      /// Speed change steps for the aft-to-forward craft axis
+      static const vec_t SPEED_DEC;
+      static const vec_t SPEED_INC;
+
+      /// Speed change step for the left-to-right craft axis
+      static const vec_t LATERAL_THRUST_RATE;
+
+      /// Speed change step for the floor-to-ceiling craft axis
+      static const vec_t VERTICAL_THRUST_RATE;
+
+      InertialPlatform state;
+
+      void vector_thrust(Vector3D thrust);
+
+	public:
+		Craft();
+		~Craft();
+
+      void go_home(void);
+      void nose_down();
+      void nose_up();
+      void roll_left();
+      void roll_right();
+      void step(void);
+      void stop(void);
+      void reverse_thrust(void);
+      void forward_thrust(void);
+      void yaw_left();
+      void yaw_right();
+      void right_thrust(void);
+      void left_thrust(void);
+      void up_thrust(void);
+      void down_thrust(void);
+
+		const InertialPlatform& get_platform() const;
+	};
+
+#endif // CRAFT_H_
