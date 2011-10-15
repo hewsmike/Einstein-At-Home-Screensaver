8d64bee07b1745b8f2da375ca2101017184f97d3
Mike Hewson <hewsmike@iinet.net.au>
2011-10-15 20:56:48 +1100

    Supernova class derived from Star
    Supernova data type

---
+++ src/solarsystem/Supernova.cpp
@@ -0,0 +1,28 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Supernova.h"
+
+Supernova::Supernova(starpos_t ra, starpos_t dec) : Star(ra, dec, "") {
+   // Base class constructor has been called.
+   }
+
+Supernova::~Supernova() {
+   }
