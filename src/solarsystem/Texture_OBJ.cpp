be41643db2693e62a86445605af48a17649a5482
Mike Hewson <hewsmike@iinet.net.au>
2011-10-15 20:55:29 +1100

    Texture_OBJ class derived from OGL_ID
    Wrapper for OpenGL server-side texture objects

---
+++ src/solarsystem/Texture_OBJ.cpp
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Texture_OBJ.h"
+
+Texture_OBJ::Texture_OBJ(void) {
+   }
+
+Texture_OBJ::~Texture_OBJ() {
+   }
+
+void Texture_OBJ::acquire(void) {
+   // Ask OpenGL to assign a texture object.
+   glGenTextures(1, &ident);
+   }
+
+void Texture_OBJ::release(void) {
+   // Ask OpenGL to release the texture object.
+   glDeleteTextures(1, &ident);
+   }
---
+++ src/solarsystem/Texture_OBJ.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef TEXTURE_OBJ_H_
+#define TEXTURE_OBJ_H_
+
+#include "OGL_ID.h"
+
+/**
+ * \addtogroup solarsystem Solarsystem
+ * @{
+ */
+
+/**
+ * \brief This interface declares public methods to deal with OpenGL
+ * texture objects. It's a wrapper.
+ *
+ * \author Mike Hewson\n
+ */
+
+class Texture_OBJ : public OGL_ID {
+   public :
+      /**
+      * \brief Constructor
+      */
+      Texture_OBJ(void);
+
+      /**
+       * \brief Destructor
+       */
+      virtual ~Texture_OBJ();
+
+      /**
+       * \brief Obtains the texture object resources.
+       */
+      void acquire(void);
+
+      /**
+       * \brief Releases the texture object resources.
+       */
+      void release(void);
+   };
+
+/**
+ * @}
+ */
+
+#endif /*TEXTURE_OBJ_H_*/
