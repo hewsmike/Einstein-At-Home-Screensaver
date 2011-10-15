12e4a0e70d02a28bb16233ad39a9f5679e7928cb
Mike Hewson <hewsmike@iinet.net.au>
2011-10-15 20:54:51 +1100

    Displaylist class derived from OGL_ID
    Wrapper for OpenGL display lists

---
+++ src/solarsystem/DisplayList.h
@@ -0,0 +1,65 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef DISPLAY_LIST_H_
+#define DISPLAY_LIST_H_
+
+#include "OGL_ID.h"
+
+/**
+ * \addtogroup solarsystem Solarsystem
+ * @{
+ */
+
+/**
+ * \brief This interface declares public methods to deal with OpenGL
+ * display lists. It's a wrapper.
+ *
+ * \author Mike Hewson\n
+ */
+
+class DisplayList : public OGL_ID {
+   public:
+      /**
+       * \brief Constructor
+       */
+      DisplayList();
+
+      /**
+       * \brief Destructor
+       */
+      virtual ~DisplayList();
+
+      /**
+       * \brief Obtains the display list resources.
+       */
+      void acquire(void);
+
+      /**
+       * \brief Releases the display list resources.
+       */
+      void release(void);
+   };
+
+/**
+ * @}
+ */
+
+#endif /*DISPLAY_LIST_H_*/
