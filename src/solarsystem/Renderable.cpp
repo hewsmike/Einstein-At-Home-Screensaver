0b02ec6f6cdab273a5030699ab1759d3e31d0f23
Mike Hewson <hewsmike@iinet.net.au>
2011-10-15 21:01:51 +1100

    Renderable class
    Base class to manage rendering of an OpenGL 3D scene element

---
+++ src/solarsystem/Renderable.cpp
@@ -0,0 +1,73 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "Renderable.h"
+
+// Mark initial state as being inactive, and lowest render quality.
+Renderable::Renderable() : activity(Renderable::INACTIVE),
+                           quality(SolarSystemGlobals::RENDER_LOWEST) {
+   // Don't call any preparation code from constructor as OpenGL context
+   // may not exist or be valid at construction time.
+   }
+
+Renderable::~Renderable() {
+   }
+
+void Renderable::activate(void) {
+   // Mark as active and prepare resources as we have decided to show it.
+   activity = Renderable::ACTIVE;
+   prepare(quality);
+   }
+
+void Renderable::inactivate(void) {
+   // Mark as inactive and release resources as we have decided not to show it.
+   activity = Renderable::INACTIVE;
+   release();
+   }
+
+Renderable::activity_state Renderable::is_activated(void) const {
+   return activity;
+   }
+
+void Renderable::toggle_activation(void) {
+	is_activated() ? inactivate() : activate();
+	}
+
+void Renderable::draw(void) {
+   // Only show if it is marked active.
+   if(activity == Renderable::ACTIVE) {
+      render();
+      }
+	}
+
+SolarSystemGlobals::render_quality Renderable::render_level(void) {
+   return quality;
+   }
+
+void Renderable::set_render_level(SolarSystemGlobals::render_quality rq) {
+   // Remember new quality setting.
+   quality = rq;
+
+   // Inactivate the item, thus releasing resources.
+   inactivate();
+
+   // Then re-activate which will trigger preparation at the new level.
+   activate();
+   }
---
+++ src/solarsystem/Renderable.h
@@ -0,0 +1,128 @@
+/***************************************************************************
+ *   Copyright (C) 2011 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef RENDERABLE_H_
+#define RENDERABLE_H_
+
+#include "SDL_opengl.h"
+
+#include "SolarSystemGlobals.h"
+
+#include <iostream>
+
+using namespace std;
+
+/**
+ * \addtogroup solarsystem Solarsystem
+ * @{
+ */
+
+/**
+ * \brief This interface declares all mandatory public methods to be provided by
+ * any given OpenGL Renderable object. One can compose a given scene element
+ * with several Renderable objects. The intent of encapsulating is to ensure
+ * proper acquisition and release of OpenGL resources. If the existing global
+ * render quality setting is altered then current resources are released,
+ * new resources are acquired as per needs and rendering continues with said
+ * resources. This class does not deal with any specific OpenGL mechanisms.
+ *
+ * \author Mike Hewson\n
+ */
+
+class Renderable {
+   public:
+      // Visibility ie. to be rendered at all?
+      enum activity_state {INACTIVE, ACTIVE};
+
+   private:
+		/// Indicates whether object is to be shown at all.
+      activity_state activity;
+
+      /// Current choice of rendering quality.
+      SolarSystemGlobals::render_quality quality;
+
+   protected:
+      /**
+	    * \brief Constructor
+	    *
+	    * The constructor is protected since this is an abstract class.
+       */
+      Renderable();
+
+      /// In subclasses this will provide OpenGL code
+      /// to prepare for rendering.
+      virtual void prepare(SolarSystemGlobals::render_quality rq) = 0;
+
+      /// In subclasses this will provide OpenGL code
+      /// to release any resources used.
+      virtual void release(void) = 0;
+
+      /// In subclasses this will provide OpenGL code
+      /// to render the object.
+      virtual void render(void) = 0;
+
+   public:
+      /// Virtual destructor
+      virtual ~Renderable();
+
+      /**
+       * \brief Invoke display of the object within the scene according to it's
+       *        activation state.
+       */
+		void draw(void);
+
+		/**
+       * \brief Mark object as to be shown
+	    */
+		void activate(void);
+
+      /**
+       * \brief Mark object as to NOT be shown
+	    */
+		void inactivate(void);
+
+      /**
+       * \brief Retrieves the activation state
+	    *
+	    * \return The activation state
+	    */
+      Renderable::activity_state is_activated() const;
+
+      /**
+       * \brief Inverts the activation state of the object
+	    */
+		void toggle_activation(void);
+
+      /**
+       * \brief Retrieves the rendering quality level
+	    */
+		SolarSystemGlobals::render_quality render_level(void);
+
+      /**
+       * \brief Sets the rendering quality level
+	    */
+		void set_render_level(SolarSystemGlobals::render_quality rq);
+   };
+
+/**
+ * @}
+ */
+
+#endif /*RENDERABLE_H_*/
