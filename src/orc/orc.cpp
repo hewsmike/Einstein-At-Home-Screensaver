741f21b165bf813ea32a47c59a895f0f4084b61b
Mike Hewson <hewsmike@iinet.net.au>
2011-11-01 12:52:20 +1100

    Code prettification ( tabs )

--- src/orc/orc.cpp
+++ src/orc/orc.cpp
@@ -27,29 +27,27 @@
 void printUsage() {
     cerr << "Invalid command-line options!" << endl;
     cerr << "Usage: orc <input filename> <output filename>" << endl;
-}
+   }
 
-int main(int argc, char *argv[])
-{
-    if(argc != 3) {
-        printUsage();
-        exit(1);
-    }
-    else {
-        string inputFilename(argv[1]);
-        string outputFilename(argv[2]);
+int main(int argc, char *argv[]) {
+   if(argc != 3) {
+      printUsage();
+      exit(1);
+      }
+   else {
+      string inputFilename(argv[1]);
+      string outputFilename(argv[2]);
 
-        // TODO: better filename checking
-        if( inputFilename == "." || inputFilename == ".." ||
-                outputFilename == "." || outputFilename == "..")
-        {
-            printUsage();
-            exit(1);
-        }
+      // TODO: better filename checking
+      if(inputFilename == "." || inputFilename == ".." ||
+         outputFilename == "." || outputFilename == "..") {
+         printUsage();
+         exit(1);
+         }
 
-        ResourceCompiler rc(inputFilename, outputFilename);
-        rc.compile();
+      ResourceCompiler rc(inputFilename, outputFilename);
+      rc.compile();
 
-        exit(0);
-    }
-}
+      exit(0);
+      }
+   }
