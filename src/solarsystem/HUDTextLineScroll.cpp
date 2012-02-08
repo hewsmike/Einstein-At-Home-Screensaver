450d78692f9881c544730b54939ba5904769a598
Mike Hewson <hewsmike@iinet.net.au>
2012-02-09 09:45:38 +1100

    Scrolling ability on HUD text lines

---
+++ src/solarsystem/HUDTextLineScroll.cpp
@@ -0,0 +1,109 @@
+/***************************************************************************
+* Copyright (C) 2012 by Mike Hewson                                        *
+* hewsmike@iinet.net.au                                                    *
+*                                                                          *
+* This file is part of Einstein@Home.                                      *
+*                                                                          *
+* Einstein@Home is free software: you can redistribute it and/or modify    *
+* it under the terms of the GNU General Public License as published        *
+* by the Free Software Foundation, version 2 of the License.               *
+*                                                                          *
+* Einstein@Home is distributed in the hope that it will be useful,         *
+* but WITHOUT ANY WARRANTY; without even the implied warranty of           *
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the             *
+* GNU General Public License for more details.                             *
+*                                                                          *
+* You should have received a copy of the GNU General Public License        *
+* along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>.    *
+*                                                                          *
+***************************************************************************/
+
+#include "HUDTextLineScroll.h"
+
+HUDTextLineScroll::HUDTextLineScroll(GLuint length, OGLFT_ft* font,
+                                     GLuint horizontalMargin, GLuint verticalMargin,
+                                     mode direction = NONE) :
+                                        HUDTextLine(length, font, horizontalMargin, verticalMargin),
+                                        dir(direction) {
+   trig.setInterval(300);
+   }
+
+HUDTextLineScroll::~HUDTextLineScroll() {
+   }
+
+HUDTextLineScroll::mode HUDTextLineScroll::getDirection(void) {
+   return dir;
+   }
+
+void HUDTextLineScroll::setDirection(mode direction = NONE) {
+   dir = direction;
+   }
+
+void HUDTextLineScroll::toggleDirection(void) {
+   switch(dir) {
+      case LEFT:
+         dir = RIGHT;
+         break;
+      case RIGHT:
+         dir = LEFT;
+         break;
+      case NONE:
+         break;
+      default:
+         // Ought not get here !!
+         std::string msg = "HUDTextLineScroll::toggleDirection() - bad switch case reached (default)";
+         ErrorHandler::record(msg, ErrorHandler::FATAL);
+         break;
+      }
+   }
+
+void HUDTextLineScroll::render(void) {
+   static const GLuint MIN_STRING_SIZE = 2;
+
+   // Has the timer triggered ?
+   if(trig.isTriggered() == TriggerTimer::TRIGGERED) {
+      // Yes, get the line's text content.
+      std::string contents = getText();
+      // It must have at last two characters
+      if(contents.size() >= MIN_STRING_SIZE) {
+         // Depending upon the scroll direction .....
+         switch(dir) {
+            case NONE:
+               // Do nothing.
+               break;
+            case LEFT:
+               // Rotate text to the left.
+               {
+                  // Get the first character.
+                  std::string first = contents.substr(0, 1);
+                  // Get the remainder.
+                  std::string remainder = contents.substr(1);
+                  // Reverse their order.
+                  contents = remainder + first;
+               }
+               break;
+            case RIGHT:
+               // Rotate text to the right.
+               {
+                  // Get the last character.
+                  std::string last = contents.substr(contents.length() - 1);
+                  // Get the remainder.
+                  std::string remainder = contents.substr(0, contents.length() - 2);
+                  // Reverse their order.
+                  contents = last + remainder;
+               }
+               break;
+            default:
+               // Ought not get here !!
+               std::string msg = "HUDTextLineScroll::render() - bad switch case reached (default)";
+               ErrorHandler::record(msg, ErrorHandler::FATAL);
+               break;
+            }
+         // Set the (new) text contents.
+         setText(contents);
+         }
+      }
+
+   // Then render this (altered) line as per base class.
+   HUDTextLine::render();
+   }
---
+++ src/solarsystem/HUDTextLineScroll.h
@@ -0,0 +1,99 @@
+/***************************************************************************
+ *   Copyright (C) 2012 by Mike Hewson                                     *
+ *   hewsmike@iinet.net.au                                                 *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef HUD_TEXT_LINE_SCROLL_H_
+#define HUD_TEXT_LINE_SCROLL_H_
+
+#include "HUDTextLine.h"
+#include "TriggerTimer.h"
+
+/**
+ * \addtogroup solarsystem Solarsystem
+ * @{
+ */
+
+/**
+ * \brief A single horizontal line of text on the HUD, which may scroll
+ *        to the left or right or not at all.
+ *
+ * \author Mike Hewson\n
+ */
+
+class HUDTextLineScroll : public HUDTextLine {
+   public:
+      /// Enumerants for the scroll behaviour.
+      enum mode {NONE, LEFT, RIGHT};
+
+      /**
+       * \brief Constructor
+       *
+       * \param length : the maximum length in characters of the text line
+       * \param font : pointer to an OGLFT font instance
+       * \param horizontalMargin : the horizontal pixel margin to be applied
+       *                           either side of the enclosed content
+       * \param verticalMargin : the vertical pixel margin to be applied
+       *                         above and below the enclosed content
+       * \param direction : the direction in which to scroll the text
+       */
+      HUDTextLineScroll(GLuint length, OGLFT_ft* font,
+                        GLuint horizontalMargin, GLuint verticalMargin,
+                        mode direction);
+
+      /**
+       * \brief Destructor
+       */
+      virtual ~HUDTextLineScroll();
+
+      /**
+       * \brief Get the direction of scroll
+       *
+       * \return the direction in which to scroll the text
+       */
+      HUDTextLineScroll::mode getDirection(void);
+
+      /**
+       * \brief Set the direction of scroll
+       *
+       * \param direction : the direction in which to scroll the text
+       */
+      void setDirection(mode direction);
+
+      /**
+       * \brief Reverse the direction of scroll
+       */
+      void toggleDirection(void);
+
+   protected:
+      /// This routine satisfies the Renderable interface.
+
+      /// Provide OpenGL code to render the object.
+      virtual void render(void);
+
+   private:
+      mode dir;
+
+      TriggerTimer trig;
+   };
+
+/**
+ * @}
+ */
+
+#endif /* HUD_TEXT_LINE_SCROLL_H_ */
