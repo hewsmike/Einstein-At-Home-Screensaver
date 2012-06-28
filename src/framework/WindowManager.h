hewsmike/Einstein-At-Home-Screensaver

commit 3b819269638bc83c08763d681827674feed4e277

Refactored SolarSystemGlobals
* Reason: removes dependency of framework on engine
* Moved getOGLVersion to WindowManager
* Moved tokeniseString to WindowManager (kind of unintuitive placement but it's only used by getOGLVersion)

src/framework/WindowManager.h
@@ -210,6 +210,25 @@ class WindowManager {
       bool setOGLContext(void);
 #endif
 
+      /**
+       * \brief Obtain the major and minor version number of the
+       *        current OpenGL context.
+       *
+       * \param major : pointer to place the major version number
+       * \param minor : pointer to place the minor version number
+       */
+      void getOGLVersion(GLuint* major, GLuint* minor);
+
+      /**
+       * \brief Tokenise a given string using a given delimiter character
+       *
+       * \param str : the string to tokenise
+       * \param delimiter : the character to delimit by
+       * \param store : a reference to the container to store the tokens in
+       *
+       */
+      void tokeniseString(const std::string str, const char delimiter, std::vector<std::string>& store);
+
       /// Local BOINC adapter instance to read project preferences
       BOINCClientAdapter *m_BoincAdapter;
 

src/framework/WindowManager.cpp
@@ -28,7 +28,6 @@
 
 #include "ErrorHandler.h"
 #include "Events.h"
-#include "SolarSystemGlobals.h"
 
 unsigned int WindowManager::OPEN_GL_VERSION_MINIMUM_MAJOR(1);
 unsigned int WindowManager::OPEN_GL_VERSION_MINIMUM_MINOR(5);
@@ -211,7 +210,7 @@ bool WindowManager::initialize(const int width, const int height, const int fram
    	// Find out the OpenGL version.
    	GLuint major = 0;
    	GLuint minor = 0;
-   	SolarSystemGlobals::getOGLVersion(&major, &minor);
+   	getOGLVersion(&major, &minor);
    	std::stringstream msg1;
    	msg1 << "WindowManager::initialize() : OpenGL version = "
    		  << major << '.' << minor;
@@ -808,7 +807,7 @@ bool WindowManager::setOGLContext(void) {
 	// Determine OpenGL version.
 	GLuint major = 0;
 	GLuint minor = 0;
-	SolarSystemGlobals::getOGLVersion(&major, &minor);
+	getOGLVersion(&major, &minor);
 	std::stringstream msg;
 	msg << "WindowManager::setOGLContext() : OpenGL v"
 	  	 << major
@@ -892,3 +891,76 @@ bool WindowManager::setOGLContext(void) {
 	return ret_val;
 	}
 #endif
+
+
+void WindowManager::getOGLVersion(GLuint* major, GLuint* minor)
+{
+    // Try to obtain the version string from the context.
+    const GLubyte* version = glGetString(GL_VERSION);
+
+    // Did that succeed ?
+    if (version == NULL) {
+        // No, can't proceed.
+        std::string msg =
+                "WindowManager::getOGLVersion() : couldn't obtain version string";
+        ErrorHandler::record(msg, ErrorHandler::FATAL);
+    }
+
+    // Convert the version string to STL format.
+    std::string ver = ErrorHandler::convertGLstring(version);
+
+    // Now tokenise the string thus obtained.
+    std::vector<std::string> ver_strings;
+    tokeniseString(ver, '.', ver_strings);
+
+    if (ver_strings.size() < 2) {
+        std::string msg =
+                "WindowManager::getOGLVersion() : couldn't tokenise version string";
+    }
+
+    int major_candidate = 0;
+    std::stringstream convert1(ver_strings[0]);
+    convert1 >> major_candidate;
+
+    if (convert1.goodbit != 0) {
+        std::string msg =
+                "WindowManager::getOGLVersion() : couldn't interpret major version string";
+        ErrorHandler::record(msg, ErrorHandler::FATAL);
+    }
+    *major = major_candidate;
+
+    int minor_candidate = 0;
+    std::stringstream convert2(ver_strings[1]);
+    convert2 >> minor_candidate;
+
+    if (convert2.goodbit != 0) {
+        std::string msg =
+                "WindowManager::getOGLVersion() : couldn't interpret minor version string";
+        ErrorHandler::record(msg, ErrorHandler::FATAL);
+    }
+    *minor = minor_candidate;
+}
+
+void WindowManager::tokeniseString(const std::string str,
+        const char delimiter, std::vector<std::string>& store)
+{
+    size_t string_pos = 0;
+    size_t delimiter_pos = 0;
+
+    // Provided we don't get to the end of ( what remains
+    // of ) the input string and not find a delimiter.
+    while ((delimiter_pos = str.find(delimiter, string_pos))
+            != std::string::npos) {
+        // Are the delimiters adjacent ? Policy is that
+        // we will not create empty tokens, hence multiple
+        // adjacent delimiters are treated as one.
+        if (delimiter_pos > string_pos) {
+            // Non-adjacent.
+            size_t token_len = delimiter_pos - string_pos;
+            std::string token = str.substr(string_pos, token_len);
+            store.push_back(token);
+        }
+        // Move along to the position just after last delimiter found.
+        string_pos = delimiter_pos + 1;
+    }
+}

src/solarsystem/SolarSystemGlobals.cpp
@@ -62,69 +62,3 @@ void SolarSystemGlobals::set_render_level(SolarSystemGlobals::render_quality rq)
 SolarSystemGlobals::render_quality SolarSystemGlobals::get_render_level(void) {
    return qual;
    }
-
-void SolarSystemGlobals::getOGLVersion(GLuint* major, GLuint* minor) {
-   // Try to obtain the version string from the context.
-   const GLubyte* version = glGetString(GL_VERSION);
-
-   // Did that succeed ?
-   if(version == NULL) {
-      // No, can't proceed.
-      std::string msg = "SolarSystemGlobals::getOGLVersion() : couldn't obtain version string";
-      ErrorHandler::record(msg, ErrorHandler::FATAL);
-      }
-
-   // Convert the version string to STL format.
-   std::string ver = ErrorHandler::convertGLstring(version);
-
-   // Now tokenise the string thus obtained.
-   std::vector<std::string> ver_strings;
-   tokeniseString(ver, '.', ver_strings);
-
-   if(ver_strings.size() < 2) {
-      std::string msg = "SolarSystemGlobals::getOGLVersion() : couldn't tokenise version string";
-      }
-
-   int major_candidate = 0;
-   std::stringstream convert1(ver_strings[0]);
-   convert1 >> major_candidate;
-
-   if(convert1.goodbit != 0) {
-      std::string msg = "SolarSystemGlobals::getOGLVersion() : couldn't interpret major version string";
-      ErrorHandler::record(msg, ErrorHandler::FATAL);
-      }
-   *major = major_candidate;
-
-   int minor_candidate = 0;
-   std::stringstream convert2(ver_strings[1]);
-   convert2 >> minor_candidate;
-
-   if(convert2.goodbit != 0) {
-      std::string msg = "SolarSystemGlobals::getOGLVersion() : couldn't interpret minor version string";
-      ErrorHandler::record(msg, ErrorHandler::FATAL);
-      }
-   *minor = minor_candidate;
-   }
-
-void SolarSystemGlobals::tokeniseString(const std::string str,
-                                        const char delimiter,
-                                        std::vector<std::string>& store) {
-   size_t string_pos = 0;
-   size_t delimiter_pos = 0;
-
-   // Provided we don't get to the end of ( what remains
-   // of ) the input string and not find a delimiter.
-   while((delimiter_pos = str.find(delimiter, string_pos)) != std::string::npos) {
-      // Are the delimiters adjacent ? Policy is that
-      // we will not create empty tokens, hence multiple
-      // adjacent delimiters are treated as one.
-      if(delimiter_pos > string_pos) {
-         // Non-adjacent.
-         size_t token_len = delimiter_pos - string_pos;
-         std::string token = str.substr(string_pos, token_len);
-         store.push_back(token);
-         }
-      // Move along to the position just after last delimiter found.
-      string_pos = delimiter_pos + 1;
-      }
-   }

src/solarsystem/SolarSystemGlobals.h
@@ -89,25 +89,6 @@ class SolarSystemGlobals {
        */
       static render_quality get_render_level(void);
 
-      /**
-       * \brief Obtain the major and minor version number of the
-       *        current OpenGL context.
-       *
-       * \param major : pointer to place the major version number
-       * \param minor : pointer to place the minor version number
-       */
-      static void getOGLVersion(GLuint* major, GLuint* minor);
-
-      /**
-       * \brief Tokenise a given string using a given delimiter character
-       *
-       * \param str : the string to tokenise
-       * \param delimiter : the character to delimit by
-       * \param store : a reference to the container to store the tokens in
-       *
-       */
-      static void tokeniseString(const std::string str, const char delimiter, std::vector<std::string>& store);
-
    private:
       /// If needed, what is the preferred OpenGL compatibility mode?
       static const GLuint PREFERRED_OGL_COMPATIBILITY_MODE_MAJOR;

commit ec5d03add7f9f65142586ce44e31c8385e2ac683

Solarsystem's main() also needs getOGLVersion() (regression in 3b819269)

src/framework/WindowManager.h
@@ -172,6 +172,15 @@ class WindowManager {
        */
       void setScreensaverMode(const bool enabled);
 
+      /**
+       * \brief Obtain the major and minor version number of the
+       *        current OpenGL context.
+       *
+       * \param major : pointer to place the major version number
+       * \param minor : pointer to place the minor version number
+       */
+      void getOGLVersion(GLuint* major, GLuint* minor);
+
    private:
 		/// Identifiers for video match results.
       enum match {MATCH_NONE, MATCH_CLOSE, MATCH_EXACT};
@@ -209,15 +218,6 @@ class WindowManager {
        */
       bool setOGLContext(void);
 #endif
-
-      /**
-       * \brief Obtain the major and minor version number of the
-       *        current OpenGL context.
-       *
-       * \param major : pointer to place the major version number
-       * \param minor : pointer to place the minor version number
-       */
-      void getOGLVersion(GLuint* major, GLuint* minor);
 
       /**
        * \brief Tokenise a given string using a given delimiter character

src/solarsystem/main.cpp
@@ -98,7 +98,7 @@ int main(int argc, char **argv) {
    // Find out the OpenGL version.
    GLuint major = 0;
    GLuint minor = 0;
-   SolarSystemGlobals::getOGLVersion(&major, &minor);
+   window.getOGLVersion(&major, &minor);
    std::stringstream msg1;
    msg1 << "SolarSystem::main() : OpenGL version = "
 		  << major << '.' << minor;