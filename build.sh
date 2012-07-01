17fc947c8ff1adfcd5961b131d5a063a9a7d26eb
Mike Hewson <hewsmike@iinet.net.au>
2012-07-01 16:15:46 +1000

    Reorganised top level build script to improve clarity
    Reordered function listing ( alphabetical within categories )
    Recategorised functions and inserted separators
    Fully enacted source version variables
    Refactored tree purging functions
    Some commenting

--- build.sh
+++ build.sh
@@ -4,6 +4,9 @@
 #   Copyright (C) 2008-2009 by Oliver Bock                                #
 #   oliver.bock[AT]aei.mpg.de                                             #
 #                                                                         #
+#   Copyright (C) 2012 by Mike Hewson  [ any & all errors are mine :-) ]  #
+#   hewsmike[AT]iinet.net.au                                              #
+#                                                                         #
 #   This file is part of Einstein@Home.                                   #
 #                                                                         #
 #   Einstein@Home is free software: you can redistribute it and/or modify #
@@ -20,7 +23,7 @@
 #                                                                         #
 ###########################################################################
 
-### globals ###############################################################
+### globals ############################################################################################################
 
 ROOT=`pwd`
 PATH_ORG="$PATH"
@@ -58,70 +61,18 @@ BS_BUILD_LIBXML_MINGW=12
 BS_BUILD_OGLFT_MINGW=13
 BS_BUILD_BOINC_MINGW=14
 
-### functions (tools) #############################################################
-
-log() {
-    echo $1 | tee -a $LOGFILE
-    }
-
-failure() {
-    log "++++++++++++++++++++++++++++++++++++"
-    log "Error detected! Stopping build!"
-    log "`date`"
-
-    if [ -f "$LOGFILE" ]; then
-        echo "------------------------------------"
-        echo "Please check logfile: `basename $LOGFILE`"
-        echo "These are the final ten lines:"
-        echo "------------------------------------"
-        tail -n 14 $LOGFILE | head -n 10
-    fi
-
-    log "++++++++++++++++++++++++++++++++++++"
-
-    exit 1
-    }
-
-distclean() {
-    cd $ROOT || failure
-
-    log "Purging build system..."
-
-    rm -rf 3rdparty || failure
-    rm -rf build || failure
-    rm -rf install || failure
-    rm -rf doc/html || failure
-    rm -f doc/*.tag || failure
-
-    rm -f .lastbuild || failure
-    rm -f .buildstate || failure
-    }
-
-check_last_build() {
-    log "Checking previous build target..."
-
-    LASTBUILD=`cat .lastbuild 2>/dev/null`
-
-    if [[ ( -f .lastbuild ) && ( "$LASTBUILD" != "$1" ) ]]; then
-        cd $ROOT || failure
-        log "Build target changed! Purging build and install trees..."
-        rm -rf build >> $LOGFILE || failure
-        rm -rf install >> $LOGFILE || failure
-        prepare_tree || failure
-    fi
-
-    echo "$1" > .lastbuild || failure
-
-    return 0
-    }
+### functions (utility) ################################################################################################
 
 check_build_state() {
     log "Checking for previous build checkpoints..."
 
+    # Do we have a record of the most recent build state?
     if [ ! -f .buildstate ]; then
+        # No, so start fromthe beginning.
         cd $ROOT || failure
         log "No previous build checkpoints found! Starting from scratch..."
     else
+        # Yes, so what is that build state?
         BUILDSTATE=`cat $ROOT/.buildstate 2>/dev/null`
         log "Recovering previous build..."
     fi
@@ -129,14 +80,25 @@ check_build_state() {
     return 0
     }
 
-store_build_state() {
-    log "Saving build checkpoint..."
-    echo "$1" > $ROOT/.buildstate || failure
+check_last_build() {
+    log "Checking previous build target..."
+
+    # Get the contents, if any, of the file recording the most recent build.
+    LASTBUILD=`cat .lastbuild 2>/dev/null`
+
+    # Assuming you have a record of the most recent build,
+    # then is it the currently requested one?
+    if [[ ( -f .lastbuild ) && ( "$LASTBUILD" != "$1" ) ]]; then
+        # No, the target has changed, purge and (re-)prepare the trees.
+        purge_tree
+        prepare_tree
+    fi
+
+    # Record the currently requested build for examination next time.
+    echo "$1" > .lastbuild || failure
 
     return 0
     }
-
-### functions (features) #############################################################
 
 check_prerequisites() {
     if [ $BUILDSTATE -ge $BS_PREREQUISITES ]; then
@@ -158,17 +120,58 @@ check_prerequisites() {
     return 0
     }
 
+distclean() {
+    cd $ROOT || failure
+
+    log "Purging build system..."
+
+    rm -rf 3rdparty || failure
+    rm -rf build || failure
+    rm -rf install || failure
+    rm -rf doc/html || failure
+    rm -f doc/*.tag || failure
+
+    rm -f .lastbuild || failure
+    rm -f .buildstate || failure
+
+    return 0
+    }
+
+failure() {
+    log "++++++++++++++++++++++++++++++++++++"
+    log "Error detected! Stopping build!"
+    log "`date`"
+
+    if [ -f "$LOGFILE" ]; then
+        echo "------------------------------------"
+        echo "Please check logfile: `basename $LOGFILE`"
+        echo "These are the final ten lines:"
+        echo "------------------------------------"
+        tail -n 14 $LOGFILE | head -n 10
+    fi
+
+    log "++++++++++++++++++++++++++++++++++++"
+
+    exit 1
+    }
+
+log() {
+    echo $1 | tee -a $LOGFILE
+    
+    return 0
+    }
+
 prepare_tree() {
     if [ $BUILDSTATE -ge $BS_PREPARE_TREE ]; then
         return 0
     fi
 
     log "Preparing tree..."
-    mkdir -p 3rdparty >> $LOGFILE || failure
-    mkdir -p install/bin >> $LOGFILE || failure
-    mkdir -p install/include >> $LOGFILE || failure
-    mkdir -p install/include/GL >> @LOGFILE || failure
-    mkdir -p install/lib >> $LOGFILE || failure
+    mkdir -p $ROOT/3rdparty >> $LOGFILE || failure
+    mkdir -p $ROOT/install/bin >> $LOGFILE || failure
+    mkdir -p $ROOT/install/include >> $LOGFILE || failure
+    mkdir -p $ROOT/install/include/GL >> @LOGFILE || failure
+    mkdir -p $ROOT/install/lib >> $LOGFILE || failure
 
     store_build_state $BS_PREPARE_TREE
     return 0
@@ -200,87 +203,61 @@ prepare_version_header() {
     echo "#endif" >> $HEADER_FILE || failure
     }
 
-prepare_glew() {
-    log "Preparing GLEW..."
-    mkdir -p $ROOT/3rdparty/glew >> $LOGFILE || failure
-     
-    cd $ROOT/3rdparty || failure
-    wget http://sourceforge.net/projects/glew/files/glew/$GLEW_VERSION/glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
-    tar -xf glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
-    rm glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
-    # substitute old source tree
-    rm -rf glew >> $LOGFILE 2>&1 || failure
-    mv glew-$GLEW_VERSION glew >> $LOGFILE 2>&1 || failure
+print_usage() {
+    cd $ROOT
 
-	return 0
-    }
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+    echo "                         Usage"
+    echo "                        -------"
+    echo
+    echo "  With two parameters ( create executables ) :"
+    echo "  ------------------------------------------"
+    echo
+    echo "              `basename $0` <target> <product>"
+    echo
+    echo "      Available targets:"
+    echo "          --linux"
+    echo "          --mac"
+    echo "          --mac-sdk           ( Mac OS 10.4 x86 SDK )"
+    echo "          --win32"
+#    echo "          --memcheck          ( assumes linux, not currently enacted )"
+#    echo "          --callgrind         ( assumes linux, not currently enacted )"
+    echo
+    echo "      Available products:"
+    echo "          --starsphere"
+    echo "          --solarsystem"
+    echo 
+    echo "  With one parameter :"
+    echo "  ------------------"
+    echo
+    echo "              `basename $0` <target>"
+    echo
+    echo "      Available targets:"
+    echo "          --distclean"
+    echo "          --doc"
+    echo
+    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-prepare_glfw() {
-    log "Preparing GLFW..."
-    mkdir -p $ROOT/3rdparty/glfw >> $LOGFILE || failure
-    mkdir -p $ROOT/build/glfw >> $LOGFILE || failure
-
-    log "Retrieving GLFW (this may take a while)..."
-    cd $ROOT/3rdparty || failure
-    wget http://sourceforge.net/projects/glfw/files/glfw/2.7.5/glfw-2.7.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    tar -xjf glfw-2.7.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    rm glfw-2.7.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    # substitute old source tree
-    rm -rf glfw >> $LOGFILE 2>&1 || failure
-    mv glfw-2.7.5 glfw >> $LOGFILE 2>&1 || failure
-
-    return 0
-    }
-
-prepare_freetype() {
-    log "Preparing Freetype2..."
-    mkdir -p $ROOT/build/freetype2 >> $LOGFILE || failure
-
-    log "Retrieving Freetype2 (this may take a while)..."
-    cd $ROOT/3rdparty || failure
-    wget http://download.savannah.gnu.org/releases/freetype/freetype-2.3.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    tar -xjf freetype-2.3.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    rm freetype-2.3.5.tar.bz2 >> $LOGFILE 2>&1 || failure
-    # substitute old source tree
-    rm -rf freetype2 >> $LOGFILE 2>&1 || failure
-    mv freetype-2.3.5 freetype2 >> $LOGFILE 2>&1 || failure
+    echo "Wrong usage. Stopping!" >> $LOGFILE
 
     return 0
     }
 
-prepare_libxml() {
-    log "Preparing libxml2..."
-    mkdir -p $ROOT/build/libxml2 >> $LOGFILE || failure
-
-    log "Retrieving libxml2 (this may take a while)..."
-    cd $ROOT/3rdparty || failure
-    rm -f libxml2-sources-2.6.32.tar.gz >> $LOGFILE 2>&1 || failure
-    wget --passive-ftp ftp://xmlsoft.org/libxml2/old/libxml2-sources-2.6.32.tar.gz >> $LOGFILE 2>&1 || failure
-    tar -xzf libxml2-sources-2.6.32.tar.gz >> $LOGFILE 2>&1 || failure
-    rm libxml2-sources-2.6.32.tar.gz >> $LOGFILE 2>&1 || failure
-    # substitute old source tree
-    rm -rf libxml2 >> $LOGFILE 2>&1 || failure
-    mv libxml2-2.6.32 libxml2 >> $LOGFILE 2>&1 || failure
+purge_tree() {
+    rm -rf $ROOT/build >> $LOGFILE || failure
+    rm -rf $ROOT/install >> $LOGFILE || failure
 
     return 0
     }
 
-prepare_oglft() {
-    log "Preparing OGLFT..."
-    mkdir -p $ROOT/3rdparty/oglft >> $LOGFILE || failure
-    mkdir -p $ROOT/build/oglft >> $LOGFILE || failure
+store_build_state() {
+    log "Saving build checkpoint..."
+    echo "$1" > $ROOT/.buildstate || failure
 
-    cd $ROOT/3rdparty/oglft || failure
-    if [ -d .svn ]; then
-        log "Updating OGLFT..."
-        # make sure local changes (patches) are reverted, hence also updated
-        svn revert -R . >> $LOGFILE  2>&1 || failure
-        svn update >> $LOGFILE  2>&1 || failure
-    else
-        log "Retrieving OGLFT (this may take a while)..."
-        svn checkout http://oglft.svn.sourceforge.net/svnroot/oglft/trunk . >> $LOGFILE 2>&1 || failure
-    fi
+    return 0
     }
+
+### functions to obtain sources ########################################################################################
 
 prepare_boinc() {
     log "Preparing BOINC..."
@@ -310,6 +287,90 @@ prepare_boinc() {
 
     return 0
     }
+
+prepare_freetype() {
+    log "Preparing Freetype2..."
+    mkdir -p $ROOT/build/freetype2 >> $LOGFILE || failure
+
+    log "Retrieving Freetype2 (this may take a while)..."
+    cd $ROOT/3rdparty || failure
+    wget http://download.savannah.gnu.org/releases/freetype/freetype-$FREETYPE_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    tar -xjf freetype-$FREETYPE_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    rm freetype-$FREETYPE_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    # substitute old source tree
+    rm -rf freetype2 >> $LOGFILE 2>&1 || failure
+    mv freetype-$FREETYPE_VERSION freetype2 >> $LOGFILE 2>&1 || failure
+
+    return 0
+    }
+
+prepare_glew() {
+    log "Preparing GLEW..."
+    mkdir -p $ROOT/3rdparty/glew >> $LOGFILE || failure
+     
+    cd $ROOT/3rdparty || failure
+    wget http://sourceforge.net/projects/glew/files/glew/$GLEW_VERSION/glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
+    tar -xf glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
+    rm glew-$GLEW_VERSION.tgz >> $LOGFILE 2>&1 || failure
+    # substitute old source tree
+    rm -rf glew >> $LOGFILE 2>&1 || failure
+    mv glew-$GLEW_VERSION glew >> $LOGFILE 2>&1 || failure
+
+	return 0
+    }
+
+prepare_glfw() {
+    log "Preparing GLFW..."
+    mkdir -p $ROOT/3rdparty/glfw >> $LOGFILE || failure
+    mkdir -p $ROOT/build/glfw >> $LOGFILE || failure
+
+    log "Retrieving GLFW (this may take a while)..."
+    cd $ROOT/3rdparty || failure
+    wget http://sourceforge.net/projects/glfw/files/glfw/$GLFW_VERSION/glfw-$GLFW_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    tar -xjf glfw-$GLFW_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    rm glfw-$GLFW_VERSION.tar.bz2 >> $LOGFILE 2>&1 || failure
+    # substitute old source tree
+    rm -rf glfw >> $LOGFILE 2>&1 || failure
+    mv glfw-$GLFW_VERSION glfw >> $LOGFILE 2>&1 || failure
+
+    return 0
+    }
+
+prepare_libxml() {
+    log "Preparing libxml2..."
+    mkdir -p $ROOT/build/libxml2 >> $LOGFILE || failure
+
+    log "Retrieving libxml2 (this may take a while)..."
+    cd $ROOT/3rdparty || failure
+    rm -f libxml2-sources-$LIBXML_VERSION.tar.gz >> $LOGFILE 2>&1 || failure
+    wget --passive-ftp ftp://xmlsoft.org/libxml2/old/libxml2-sources-$LIBXML_VERSION.tar.gz >> $LOGFILE 2>&1 || failure
+    tar -xzf libxml2-sources-$LIBXML_VERSION.tar.gz >> $LOGFILE 2>&1 || failure
+    rm libxml2-sources-$LIBXML_VERSION.tar.gz >> $LOGFILE 2>&1 || failure
+    # substitute old source tree
+    rm -rf libxml2 >> $LOGFILE 2>&1 || failure
+    mv libxml2-$LIBXML_VERSION libxml2 >> $LOGFILE 2>&1 || failure
+
+    return 0
+    }
+
+prepare_oglft() {
+    log "Preparing OGLFT..."
+    mkdir -p $ROOT/3rdparty/oglft >> $LOGFILE || failure
+    mkdir -p $ROOT/build/oglft >> $LOGFILE || failure
+
+    cd $ROOT/3rdparty/oglft || failure
+    if [ -d .svn ]; then
+        log "Updating OGLFT..."
+        # make sure local changes (patches) are reverted, hence also updated
+        svn revert -R . >> $LOGFILE  2>&1 || failure
+        svn update >> $LOGFILE  2>&1 || failure
+    else
+        log "Retrieving OGLFT (this may take a while)..."
+        svn checkout http://oglft.svn.sourceforge.net/svnroot/oglft/trunk . >> $LOGFILE 2>&1 || failure
+    fi
+    }
+
+### functions to build from sources for linux targets #################################################################
 
 build_glew() {
     if [ $BUILDSTATE -ge $BS_BUILD_GLEW ]; then
@@ -341,26 +402,6 @@ build_glew() {
     return 0
     }
 
-build_glew_mac() {
-    if [ $BUILDSTATE -ge $BS_BUILD_GLEW ]; then
-        return 0
-    fi
-      
-    prepare_glew || failure
-      
-    log "Building GLEW (this may take a while)..."
-      
-    cd $ROOT/3rdparty/glew
-    GLEW_DEST="$ROOT/install" make install >> $LOGFILE 2>&1 || failure
-    # don't use shared GLEW libraries   
-    rm -f $ROOT/install/lib/libGLEW*.dylib
-   
-    log "Successfully built and installed GLEW!"
-      
-    store_build_state $BS_BUILD_GLEW || failure
-    return 0
-    }
-
 build_glfw() {
     if [ $BUILDSTATE -ge $BS_BUILD_GLFW ]; then
         return 0
@@ -374,26 +415,6 @@ build_glfw() {
     export PREFIX=$ROOT/install
 
     make x11-install >> $LOGFILE 2>&1 || failure
-
-    log "Successfully built and installed GLFW!"
-
-    store_build_state $BS_BUILD_GLFW || failure
-    return 0
-    }
-
-build_glfw_mac() {
-    if [ $BUILDSTATE -ge $BS_BUILD_GLFW ]; then
-        return 0
-    fi
-
-    prepare_glfw || failure
-
-    log "Building GLFW (this may take a while)..."
-    cd $ROOT/3rdparty/glfw || failure
-
-    export PREFIX=$ROOT/install
-
-    make cocoa-install >> $LOGFILE 2>&1 || failure
 
     log "Successfully built and installed GLFW!"
 
@@ -502,6 +523,50 @@ build_boinc() {
     store_build_state $BS_BUILD_BOINC || failure
     return 0
     }
+
+### functions to build from sources for Mac targets ####################################################################
+
+build_glew_mac() {
+    if [ $BUILDSTATE -ge $BS_BUILD_GLEW ]; then
+        return 0
+    fi
+      
+    prepare_glew || failure
+      
+    log "Building GLEW (this may take a while)..."
+      
+    cd $ROOT/3rdparty/glew
+    GLEW_DEST="$ROOT/install" make install >> $LOGFILE 2>&1 || failure
+    # don't use shared GLEW libraries   
+    rm -f $ROOT/install/lib/libGLEW*.dylib
+   
+    log "Successfully built and installed GLEW!"
+      
+    store_build_state $BS_BUILD_GLEW || failure
+    return 0
+    }
+
+build_glfw_mac() {
+    if [ $BUILDSTATE -ge $BS_BUILD_GLFW ]; then
+        return 0
+    fi
+
+    prepare_glfw || failure
+
+    log "Building GLFW (this may take a while)..."
+    cd $ROOT/3rdparty/glfw || failure
+
+    export PREFIX=$ROOT/install
+
+    make cocoa-install >> $LOGFILE 2>&1 || failure
+
+    log "Successfully built and installed GLFW!"
+
+    store_build_state $BS_BUILD_GLFW || failure
+    return 0
+    }
+
+### functions to build from sources for Win32 targets ##################################################################
 
 set_mingw() {
     # general config
@@ -673,6 +738,8 @@ build_boinc_mingw() {
     return 0
     }
 
+### generic build function #############################################################################################
+
 build_product() {
     # make sure ORC is always compiled for host platform (it's executed during $PRODUCT_NAME build!)
     log "Preparing $PRODUCT_NAME..."
@@ -743,6 +810,8 @@ build_product() {
     return 0
     }
 
+### specific build functions ###########################################################################################
+
 build_linux() {
     log "Important for an official build: let CC and CXX point to gcc/g++ 4.6+ !"
     build_glew || failure
@@ -785,47 +854,9 @@ build_win32() {
     return 0
     }
 
-print_usage() {
-    cd $ROOT
+### main control #######################################################################################################
 
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
-    echo "                         Usage"
-    echo "                        -------"
-    echo
-    echo "  With two parameters ( create executables ) :"
-    echo "  ------------------------------------------"
-    echo
-    echo "              `basename $0` <target> <product>"
-    echo
-    echo "      Available targets:"
-    echo "          --linux"
-    echo "          --mac"
-    echo "          --mac-sdk           ( Mac OS 10.4 x86 SDK )"
-    echo "          --win32"
-#    echo "          --memcheck          ( assumes linux, not currently enacted )"
-#    echo "          --callgrind         ( assumes linux, not currently enacted )"
-    echo
-    echo "      Available products:"
-    echo "          --starsphere"
-    echo "          --solarsystem"
-    echo 
-    echo "  With one parameter :"
-    echo "  ------------------"
-    echo
-    echo "              `basename $0` <target>"
-    echo
-    echo "      Available targets:"
-    echo "          --distclean"
-    echo "          --doc"
-    echo
-    echo "++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
 
-    echo "Wrong usage. Stopping!" >> $LOGFILE
-
-    return 0
-    }
-
-### main control ##########################################################
 
 # Delete any prior build log
 rm -f ./build.log
