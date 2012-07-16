1b8e3849693530df699199edc1dfe44dcf478dbf
Mike Hewson <hewsmike@iinet.net.au>
2012-07-16 13:10:55 +1000

    HUDContainer class prettification

--- src/solarsystem/HUDContainer.cpp
+++ src/solarsystem/HUDContainer.cpp
@@ -25,194 +25,194 @@
 #include "ErrorHandler.h"
 
 HUDContainer::HUDContainer(HUDContainer* enclosing = NULL) :
-              wd(0),
-              ht(0) {
-   }
+                wd(0),
+                ht(0) {
+    }
 
 HUDContainer::~HUDContainer() {
-   }
+    }
 
 bool HUDContainer::requestResize(GLuint newWidth, GLuint newHeight) {
-   // Assume failure.
-   bool ret_val = false;
+    // Assume failure.
+    bool ret_val = false;
 
-   // Are the requested dimensions above or equal to the minimum allowed ?
-   if((newWidth >= minWidth()) && (newHeight >= minHeight())) {
-      // Yes, set those valid dimensions ....
-      setDimensions(newWidth, newHeight);
-      // and then rebase the panel's contents.
-      allocateItemBases();
-      // Indicate success.
-      ret_val = true;
-      }
+    // Are the requested dimensions above or equal to the minimum allowed ?
+    if((newWidth >= minWidth()) && (newHeight >= minHeight())) {
+        // Yes, set those valid dimensions ....
+        setDimensions(newWidth, newHeight);
+        // and then rebase the panel's contents.
+        allocateItemBases();
+        // Indicate success.
+        ret_val = true;
+        }
 
-   return ret_val;
-   }
+    return ret_val;
+    }
 
 void HUDContainer::reBase(GLuint newHorz, GLuint newVert) {
-   // Rebase this container and then ...
-   HUDItem::reBase(newHorz, newVert);
+    // Rebase this container and then ...
+    HUDItem::reBase(newHorz, newVert);
 
-   // ... rebase the contained items.
-   allocateItemBases();
-   }
+    // ... rebase the contained items.
+    allocateItemBases();
+    }
 
 GLuint HUDContainer::itemCount(void) const {
-   return container.size();
-   }
+    return container.size();
+    }
 
 GLuint HUDContainer::width(void) const {
-   return wd;
-   }
+    return wd;
+    }
 
 GLuint HUDContainer::height(void) const {
-   return ht;
-   }
+    return ht;
+    }
 
 bool HUDContainer::isEmpty(void) const {
-   return container.empty();
-   }
+    return container.empty();
+    }
 
 void HUDContainer::emptyContainer(void) {
-   container.clear();
-   }
+    container.clear();
+    }
 
 void HUDContainer::adjust() {
-   // Reassess this container's correct/current minimal dimensions.
-   std::pair<GLuint, GLuint> min_size = reassessMinimumDimensions();
+    // Reassess this container's correct/current minimal dimensions.
+    std::pair<GLuint, GLuint> min_size = reassessMinimumDimensions();
 
-   // Call base class to adjust to such minima.
-   this->setMinimumDimensions(min_size.first, min_size.second);
+    // Call base class to adjust to such minima.
+    this->setMinimumDimensions(min_size.first, min_size.second);
 
-   // However the 'outer' dimensions may need to expand because
-   // either/both of the minimum height/width was increased. In
-   // which case set those outer dimensions to the minima.
-   if(min_size.first > width()) {
-      setDimensions(min_size.first, height());
-      }
-   if(min_size.second > height()) {
-      setDimensions(width(), min_size.second);
-      }
+    // However the 'outer' dimensions may need to expand because
+    // either/both of the minimum height/width was increased. In
+    // which case set those outer dimensions to the minima.
+    if(min_size.first > width()) {
+        setDimensions(min_size.first, height());
+        }
+    if(min_size.second > height()) {
+        setDimensions(width(), min_size.second);
+        }
 
-   // Rebase the contained items.
-   allocateItemBases();
+    // Rebase the contained items.
+    allocateItemBases();
 
-   // Notify any possible containing object of this size change.
-   HUDContainer* outer = getEnclosingContainer();
-   if(outer !=NULL) {
-      outer->adjust();
-      }
-   }
+    // Notify any possible containing object of this size change.
+    HUDContainer* outer = getEnclosingContainer();
+    if(outer !=NULL) {
+        outer->adjust();
+        }
+    }
 
 void HUDContainer::addItem(int handle, HUDItem* item) {
-   // Make sure a NULL pointer was not passed.
-   if(item == NULL) {
-      // A significant error & ought not go un-managed.
-      ErrorHandler::record("HUDContainer::addObject() - NULL passed for object!",
-                           ErrorHandler::FATAL);
-      }
+    // Make sure a NULL pointer was not passed.
+    if(item == NULL) {
+        // A significant error & ought not go un-managed.
+        ErrorHandler::record("HUDContainer::addObject() - NULL passed for object!",
+                             ErrorHandler::FATAL);
+        }
 
-   // If the given handle is not present in the map, nothing happens.
-   container.erase(handle);
+    // If the given handle is not present in the map, nothing happens.
+    container.erase(handle);
 
-   // Then put in a new key/value combo using pair semantics.
-   container.insert(std::pair<int, HUDItem*>(handle, item));
+    // Then put in a new key/value combo using pair semantics.
+    container.insert(std::pair<int, HUDItem*>(handle, item));
 
-   // Allow this container to be the enclosing one for this included item.
-   item->setEnclosingContainer(this);
+    // Allow this container to be the enclosing one for this included item.
+    item->setEnclosingContainer(this);
 
-   // Adjust container to account for this addition.
-   adjust();
-   }
+    // Adjust container to account for this addition.
+    adjust();
+    }
 
 HUDItem* HUDContainer::getItem(int handle) const {
-   // Default return value indicates nothing found.
-   HUDItem* ret_val = NULL;
+    // Default return value indicates nothing found.
+    HUDItem* ret_val = NULL;
 
-   // Let's see if we can find an item with the given handle.
-   std::map<int, HUDItem*>::const_iterator pos = container.find(handle);
+    // Let's see if we can find an item with the given handle.
+    std::map<int, HUDItem*>::const_iterator pos = container.find(handle);
 
-   // Was it recovered?
-   if(pos != container.end()) {
-      // Yes, it was there. So the other member of the pair at
-      // that position is ( the pointer to ) our desired item.
-      ret_val = pos->second;
-      }
+    // Was it recovered?
+    if(pos != container.end()) {
+        // Yes, it was there. So the other member of the pair at
+        // that position is ( the pointer to ) our desired item.
+        ret_val = pos->second;
+        }
 
-   return ret_val;
-   }
+    return ret_val;
+    }
 
 void HUDContainer::setDimensions(GLuint newWidth, GLuint newHeight) {
-   // Set the new dimensions.
-   wd = newWidth;
-   ht = newHeight;
-   }
+    // Set the new dimensions.
+    wd = newWidth;
+    ht = newHeight;
+    }
 
 void HUDContainer::prepare(SolarSystemGlobals::render_quality rq) {
-   // Policy is that when a container is activated, then all
-   // contained items are as well.
+    // Policy is that when a container is activated, then all
+    // contained items are as well.
 
-   // Go through the container, preparing each item within.
-   for(std::map<int, HUDItem*>::iterator obj = container.begin();
-       obj != container.end();
-       ++obj) {
-      // Mechanics of Renderable class ensures that activation
-      // implies preparation.
+    // Go through the container, preparing each item within.
+    for(std::map<int, HUDItem*>::iterator obj = container.begin();
+        obj != container.end();
+        ++obj) {
+        // Mechanics of Renderable class ensures that activation
+        // implies preparation.
 
-      // Check on nullity ie. assume the iterator is valid, but
-      // does it dereference to a NULL pointer ?
-      if(obj->second != NULL) {
-         obj->second->activate();
-         }
-      else {
-         // Shouldn't ever get here, but ...
-         ErrorHandler::record("HUDContainer::prepare() - NULL found for object!",
-                              ErrorHandler::FATAL);
-         }
-      }
-   }
+        // Check on nullity ie. assume the iterator is valid, but
+        // does it dereference to a NULL pointer ?
+        if(obj->second != NULL) {
+            obj->second->activate();
+            }
+        else {
+            // Shouldn't ever get here, but ...
+            ErrorHandler::record("HUDContainer::prepare() - NULL found for object!",
+                                  ErrorHandler::FATAL);
+            }
+        }
+
 
 void HUDContainer::release(void) {
-   // Policy is that when a container is inactivated, then all
-   // contained items are as well.
+    // Policy is that when a container is inactivated, then all
+    // contained items are as well.
 
-   // Go through the container, releasing each item within.
-   for(std::map<int, HUDItem*>::iterator obj = container.begin();
-       obj != container.end();
-       ++obj) {
-      // Similiar to prepare() above, as inactivation has the same
-      // relation to release.
+    // Go through the container, releasing each item within.
+    for(std::map<int, HUDItem*>::iterator obj = container.begin();
+        obj != container.end();
+        ++obj) {
+        // Similiar to prepare() above, as inactivation has the same
+        // relation to release.
 
-      // Check on nullity ie. assume the iterator is valid, but
-      // does it dereference to a NULL pointer ?
-      if(obj->second != NULL) {
-         obj->second->inactivate();
-         }
-      else {
-         // Shouldn't ever get here, but ...
-         ErrorHandler::record("HUDContainer::release() - NULL found for object!",
-                              ErrorHandler::FATAL);
-         }
-      }
-   }
+        // Check on nullity ie. assume the iterator is valid, but
+        // does it dereference to a NULL pointer ?
+        if(obj->second != NULL) {
+            obj->second->inactivate();
+            }
+        else {
+            // Shouldn't ever get here, but ...
+            ErrorHandler::record("HUDContainer::release() - NULL found for object!",
+                                 ErrorHandler::FATAL);
+            }
+        }
+    }
 
 void HUDContainer::render(void) {
-   // Go through the container, rendering each item within.
-   for(std::map<int, HUDItem*>::iterator obj = container.begin();
-       obj != container.end();
-       ++obj) {
-      // Similiar to prepare() and release() above, drawing has
-      // the same relation to rendering.
+    // Go through the container, rendering each item within.
+    for(std::map<int, HUDItem*>::iterator obj = container.begin();
+        obj != container.end();
+        ++obj) {
+        // Similiar to prepare() and release() above, drawing has
+        // the same relation to rendering.
 
-      // Check on nullity ie. assume the iterator is valid, but
-      // does it dereference to a NULL pointer ?
-      if(obj->second != NULL) {
-         obj->second->draw();
-         }
-      else {
-         // Shouldn't ever get here, but ...
-         ErrorHandler::record("HUDContainer::render() - NULL found for object!",
-                              ErrorHandler::FATAL);
-         }
-      }
-   }
+        // Check on nullity ie. assume the iterator is valid, but
+        // does it dereference to a NULL pointer ?
+        if(obj->second != NULL) {
+            obj->second->draw();
+            }
+        else {
+            // Shouldn't ever get here, but ...
+            ErrorHandler::record("HUDContainer::render() - NULL found for object!",
+                                 ErrorHandler::FATAL);
+            }
+        }
+    }
--- src/solarsystem/HUDContainer.h
+++ src/solarsystem/HUDContainer.h
@@ -34,158 +34,162 @@
 /**
  * \brief Base container type for display of HUD content.
  *
- * The key design issue here is that a HUDContainer may be
- * one/both of container and contained.
+ *      The key design issue here is that a HUDContainer
+ * may be one/both of container and contained, meaning that
+ * a HUDContainer may contain another HUDContainer.
+ *
+ * \see HUDItem
  *
  * \author Mike Hewson\n
  */
 
 class HUDContainer : public HUDItem {
-   public:
-      /**
-       * \brief Constructor
-       */
-      HUDContainer(HUDContainer* enclosing);
+    public:
+        /**
+         * \brief Constructor
+         */
+        HUDContainer(HUDContainer* enclosing);
 
-      /**
-       * \brief Destructor
-       */
-      virtual ~HUDContainer();
+        /**
+         * \brief Destructor
+         */
+        virtual ~HUDContainer();
 
-      /**
-       * \brief Resize this container, at or above it's minimum dimensions
-       *
-       * \param newWidth : the desired width
-       * \param newHeight : the desired height
-       *
-       * \return bool : whether the resize occured
-       *                - true if a resize was allowed
-       *                - false if below minimum was requested
-       */
-      bool requestResize(GLuint newWidth, GLuint newHeight);
+        /**
+         * \brief Resize this container, at or above it's minimum dimensions
+         *
+         * \param newWidth : the desired width
+         * \param newHeight : the desired height
+         *
+         * \return bool : whether the resize occured
+         *                - true if a resize was allowed
+         *                - false if below minimum was requested
+         */
+        bool requestResize(GLuint newWidth, GLuint newHeight);
 
-      /**
-       * \brief Set the lower left corner window coordinates
-       *
-       * \param newHorz - the desired horizontal coordinate
-       * \param newVert - the desired vertical coordinate
-       */
-      void reBase(GLuint newHorz, GLuint newVert);
+        /**
+         * \brief Set the lower left corner window coordinates
+         *
+         * \param newHorz - the desired horizontal coordinate
+         * \param newVert - the desired vertical coordinate
+         */
+        void reBase(GLuint newHorz, GLuint newVert);
 
-      /**
-       * \brief Re-evaluate this container's sizing and contained items'
-       *        positions. Triggered by a change within the container.
-       */
-      void adjust(void);
+        /**
+         * \brief Re-evaluate this container's sizing and contained items'
+         *        positions. Triggered by a change within the container.
+         */
+        void adjust(void);
 
-      /**
-       * \brief Empty the underlying container.
-       */
-      void emptyContainer(void);
+        /**
+         * \brief Empty the underlying container.
+         */
+        void emptyContainer(void);
 
-   protected:
-       /**
-       * \brief Obtain the count of contained items
-       *
-       * \return the count
-       */
-      GLuint itemCount(void) const;
+    protected:
+        /**
+         * \brief Obtain the count of contained items
+         *
+         * \return the count
+         */
+        GLuint itemCount(void) const;
 
-       /**
-       * \brief Obtain the container's current width
-       *
-       * \return the width
-       */
-      GLuint width(void) const;
+        /**
+         * \brief Obtain the container's current width
+         *
+         * \return the width
+         */
+        GLuint width(void) const;
 
-      /**
-       * \brief Obtain the container's current height
-       *
-       * \return the height
-       */
-      GLuint height(void) const;
+        /**
+         * \brief Obtain the container's current height
+         *
+         * \return the height
+         */
+        GLuint height(void) const;
 
-      /**
-       * \brief Query whether anything is contained within
-       *
-       * \return boolean : indicating true if empty, false otherwise
-       */
-      bool isEmpty(void) const;
+        /**
+         * \brief Query whether anything is contained within
+         *
+         * \return boolean : indicating true if empty, false otherwise
+         */
+        bool isEmpty(void) const;
 
-      /**
-       * \brief Get a pointer to a contained item
-       *
-       * \param handle : the identifier used when the item was added
-       *
-       * \return HUDItem* : a pointer to the item ( NULL if not present )
-       */
-      HUDItem* getItem(int handle) const;
+        /**
+         * \brief Get a pointer to a contained item
+         *
+         * \param handle : the identifier used when the item was added
+         *
+         * \return HUDItem* : a pointer to the item ( NULL if not present )
+         */
+        HUDItem* getItem(int handle) const;
 
-       /**
-       * \brief Add an item to this container.
-       *
-       * If this does not create a FATAL error when executing, then on
-       * completion :
-       *    - the given pointer was not NULL, and
-       *    - the item was inserted, and
-       *    - any existing item with the same handle was deleted, and
-       *    - this container's minimum dimensions were adjusted
-       *
-       * \param handle : an identifier for the item
-       * \param obj : the item's pointer
-        */
-      void addItem(int handle, HUDItem* item);
+        /**
+         * \brief Add an item to this container.
+         *
+         *      IF this does not create a FATAL error when executing,
+         * then on completion :
+         *    - the given pointer was not NULL, and
+         *    - the item was inserted, and
+         *    - any existing item with the same handle was deleted, and
+         *    - this container's minimum dimensions were adjusted
+         *
+         * \param handle : an identifier for the item
+         * \param obj : the item's pointer
+         */
+        void addItem(int handle, HUDItem* item);
 
-      /**
-       * \brief Re-assess the minimal width and height.
-       *
-       * As this depends upon the container's internal layout,
-       * then a subclass must enact this.
-       *
-       * \return std::pair composed of ( in order )
-       *          - the minimum width
-       *          - the minimum height
-       */
-      virtual std::pair<GLuint, GLuint> reassessMinimumDimensions(void) = 0;
+        /**
+         * \brief Re-assess the minimal width and height.
+         *
+         *      Pure virtual as this depends upon the container's
+         * internal layout, and a subclass must enact this.
+         *
+         * \return std::pair composed of ( in order )
+         *          - the minimum width
+         *          - the minimum height
+         */
+        virtual std::pair<GLuint, GLuint> reassessMinimumDimensions(void) = 0;
 
-      /**
-       * \brief Determine and set the base positions of each item. Pure
-       *        virtual as only a subclass will know how to do this.
-       */
-      virtual void allocateItemBases(void) = 0;
+        /**
+         * \brief Determine and set the base positions of each item.
+         *
+         *      Pure virtual as only a subclass will know how to do this.
+         */
+        virtual void allocateItemBases(void) = 0;
 
-      /**
-       * \brief Unconditionally set the size of this container.
-       *
-       * \param newWidth : the desired width
-       * \param newHeight : the desired height
-       */
-      void setDimensions(GLuint newWidth, GLuint newHeight);
+        /**
+         * \brief Unconditionally set the size of this container.
+         *
+         * \param newWidth : the desired width
+         * \param newHeight : the desired height
+         */
+        void setDimensions(GLuint newWidth, GLuint newHeight);
 
-      /// These three routines below satisfy the Renderable interface.
+        /// These three routines below satisfy the Renderable interface.
 
-      /// Provide OpenGL code to prepare for rendering.
-      virtual void prepare(SolarSystemGlobals::render_quality rq);
+        /// Provide OpenGL code to prepare for rendering.
+        virtual void prepare(SolarSystemGlobals::render_quality rq);
 
-      /// Provide OpenGL code to release any resources used.
-      virtual void release(void);
+        /// Provide OpenGL code to release any resources used.
+        virtual void release(void);
 
-      /// Provide OpenGL code to render the object.
-      virtual void render(void);
+        /// Provide OpenGL code to render the object.
+        virtual void render(void);
 
-   private:
-      /// The actual container within. Yes I'd like to do references,
-      /// but the STL container classes imply assignable elements ...
-      /// ..... I can't even have a const pointer! :-(
-      /// The key is going to store an integral cast of an enumerator.
-      std::map<int, HUDItem*> container;
+    private:
+        /// The actual container within. Yes I'd like to do references,
+        /// but the STL container classes imply assignable elements ...
+        /// ..... I can't even have a const pointer! :-(
+        /// The key is going to store an integral cast of an enumerator.
+        std::map<int, HUDItem*> container;
 
-      /// Current actual dimensions ie. reflects the amounts above it's
-      /// minima, and are thus available to distribute b/w contents as
-      /// justification.
-      GLuint wd;
-      GLuint ht;
-   };
+        /// Current actual dimensions ie. reflects the amounts above it's
+        /// minima, and are thus available to distribute b/w contents as
+        /// justification.
+        GLuint wd;
+        GLuint ht;
+    };
 
 /**
  * @}
