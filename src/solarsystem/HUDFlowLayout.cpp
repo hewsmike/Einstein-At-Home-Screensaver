64689c525ca568455a932f35627277885261886e
Mike Hewson <hewsmike@iinet.net.au>
2012-07-17 16:12:43 +1000

    HUDFlowLayout class prettification

--- src/solarsystem/HUDFlowLayout.cpp
+++ src/solarsystem/HUDFlowLayout.cpp
@@ -23,189 +23,188 @@
 #include "ErrorHandler.h"
 
 HUDFlowLayout::HUDFlowLayout(HUDContainer* enclosing, Axis axis) :
-               HUDContainer(enclosing),
-               ax(axis) {
-   gap_count = 0;
-   total_white_space = 0;
-   start_offset = 0;
-   item_gap = 0;
-   primary_just = CENTRE;
-   secondary_just = MIDDLE;
-   }
+                HUDContainer(enclosing),
+                ax(axis) {
+    gap_count = 0;
+    total_white_space = 0;
+    start_offset = 0;
+    item_gap = 0;
+    primary_just = CENTRE;
+    secondary_just = MIDDLE;
+    }
 
 HUDFlowLayout::~HUDFlowLayout() {
-   }
+    }
 
 void HUDFlowLayout::addContent(HUDContent* content) {
-   // Only add if NULL wasn't passed.
-   if(content != NULL) {
-      // Just index these by their order of being added.
-      addItem(itemCount(), content);
-      }
-   else {
-      ErrorHandler::record("HUDFlowLayout::addContent() - NULL passed for content",
-                            ErrorHandler::FATAL);
-      }
-   }
+    // Only add if NULL wasn't passed.
+    if(content != NULL) {
+        // Just index these by their order of being added.
+        addItem(itemCount(), content);
+        }
+    else {
+        ErrorHandler::record("HUDFlowLayout::addContent() - NULL passed for content",
+                             ErrorHandler::FATAL);
+        }
+    }
 
 void HUDFlowLayout::setPrimaryJustification(primaryJustification justification) {
-   primary_just = justification;
-   allocateItemBases();
-   }
+    primary_just = justification;
+    allocateItemBases();
+    }
 
 void HUDFlowLayout::setSecondaryJustification(secondaryJustification justification) {
-   secondary_just = justification;
-   allocateItemBases();
-   }
+    secondary_just = justification;
+    allocateItemBases();
+    }
 
 HUDFlowLayout::primaryJustification HUDFlowLayout::getPrimaryJustification(void) const {
-   return primary_just;
-   }
+    return primary_just;
+    }
 
 HUDFlowLayout::secondaryJustification HUDFlowLayout::getSecondaryJustification(void) const {
-   return secondary_just;
-   }
+    return secondary_just;
+    }
 
 std::pair<GLuint, GLuint> HUDFlowLayout::reassessMinimumDimensions(void) {
-   // Assume an empty container.
-   GLuint layoutMinWidth = 0;
-   GLuint layoutMinHeight = 0;
+    // Assume an empty container.
+    GLuint layoutMinWidth = 0;
+    GLuint layoutMinHeight = 0;
 
-   // How many items are contained?
-   GLuint num_items = itemCount();
+    // How many items are contained?
+    GLuint num_items = itemCount();
 
-   // Go through and access each one.
-   for(GLuint count = 0; count < num_items; ++count) {
-      // Examine this item.
-      HUDItem* current = getItem(count);
+    // Go through and access each one.
+    for(GLuint count = 0; count < num_items; ++count) {
+        // Examine this item.
+        HUDItem* current = getItem(count);
 
-      // We add up all the minimum widths.
-      layoutMinWidth += current->minWidth();
+        // We add up all the minimum widths.
+        layoutMinWidth += current->minWidth();
 
-      // But take the overall minimum height to be the
-      // greatest of the minima of the contained items.
-      GLuint currentHeight = current->minHeight();
-      if(currentHeight > layoutMinHeight) {
-         layoutMinHeight = currentHeight;
-         }
-      }
+        // But take the overall minimum height to be the
+        // greatest of the minima of the contained items.
+        GLuint currentHeight = current->minHeight();
+        if(currentHeight > layoutMinHeight) {
+            layoutMinHeight = currentHeight;
+            }
+        }
 
-   return std::pair<GLuint, GLuint>(layoutMinWidth, layoutMinHeight);
-   }
+    return std::pair<GLuint, GLuint>(layoutMinWidth, layoutMinHeight);
+    }
 
 void HUDFlowLayout::allocateItemBases(void) {
-   // TODO write cases to justify along the secondary axis.
+    /// TODO write cases to justify along the secondary axis.
 
-   // We only need to bother with this if there are items to render.
-   if(itemCount() > 0) {
-      setGaps();
-      // Starting position.
-      GLuint newHorz = horzBase();
-      GLuint newVert = vertBase();
+    // We only need to bother with this if there are items to render.
+    if(itemCount() > 0) {
+        setGaps();
+        // Starting position.
+        GLuint newHorz = horzBase();
+        GLuint newVert = vertBase();
 
-      // Offset added as per flow axis.
-      switch(ax) {
-      	case HORIZONTAL:
-      		newHorz += start_offset;
-      		break;
-      	case VERTICAL:
-      		newVert += start_offset;
-      		break;
-      	default:
-      		// Shouldn't ever get here!!
-      		ErrorHandler::record("HUDFlowLayout::allocateItemBases() - bad switch case reached (default)",
-      									ErrorHandler::FATAL);
-      		break;
-      	}
+        // Offset added as per flow axis.
+        switch(ax) {
+            case HORIZONTAL:
+                newHorz += start_offset;
+                break;
+            case VERTICAL:
+                newVert += start_offset;
+                break;
+            default:
+                // Shouldn't ever get here!!
+                ErrorHandler::record("HUDFlowLayout::allocateItemBases() - bad switch case reached (default)",
+                                     ErrorHandler::FATAL);
+                break;
+            }
 
-      // Then contents are placed in the order of their insertion.
-      for(GLuint count = 0; count < itemCount(); ++count) {
-      	// Place the current content.
-      	getItem(count)->reBase(newHorz, newVert);
+        // Then contents are placed in the order of their insertion.
+        for(GLuint count = 0; count < itemCount(); ++count) {
+            // Place the current content.
+            getItem(count)->reBase(newHorz, newVert);
 
-      	// Shift to next insert position by the dimensions of this content plus any gap,
-      	// accounting for the flow axis.
-      	switch(ax) {
-      		case HORIZONTAL:
-      			newHorz += getItem(count)->minWidth() + item_gap;
-      			break;
-      	   case VERTICAL:
-      	   	newVert += getItem(count)->minHeight() + item_gap;;
-      	      break;
-      	   default:
-      	   	// Shouldn't ever get here!!
-      	      ErrorHandler::record("HUDFlowLayout::allocateItemBases() - bad switch case reached (default)",
-      	      							ErrorHandler::FATAL);
-      	      break;
-      		}
-      	}
-      }
-   }
-
+            // Shift to next insert position by the dimensions of this content plus any gap,
+            // accounting for the flow axis.
+            switch(ax) {
+                case HORIZONTAL:
+                    newHorz += getItem(count)->minWidth() + item_gap;
+                    break;
+                case VERTICAL:
+                    newVert += getItem(count)->minHeight() + item_gap;;
+                    break;
+                default:
+                    // Shouldn't ever get here!!
+                    ErrorHandler::record("HUDFlowLayout::allocateItemBases() - bad switch case reached (default)",
+                                         ErrorHandler::FATAL);
+                break;
+                }
+            }
+        }
+    }
 
 void HUDFlowLayout::setGaps(void) {
-   // Determine any available horizontal 'whitespace' b/w items. This is
-   // the amount above the minimum required for display. By design this
-   // whitespace ought be non-negative, but let's check to be sure.
-   if(width() >= minWidth()) {
-      total_white_space = width() - minWidth();
-      }
-   else {
-      ErrorHandler::record("HUDFlowLayout::setGaps() - negative whitespace!!",
-                           ErrorHandler::FATAL);
-      }
+    // Determine any available horizontal 'whitespace' b/w items. This is
+    // the amount above the minimum required for display. By design this
+    // whitespace ought be non-negative, but let's check to be sure.
+    if(width() >= minWidth()) {
+        total_white_space = width() - minWidth();
+        }
+    else {
+        ErrorHandler::record("HUDFlowLayout::setGaps() - negative whitespace!!",
+                             ErrorHandler::FATAL);
+        }
 
-   // For the items, what is the distribution of whitespace?
-   // This depends upon the chosen primary justification style.
-   switch(getPrimaryJustification()) {
-      case START:
-         // There's only a single gap on the other side to the justification.
-         gap_count = 1;
-         item_gap = 0;
-         start_offset = 0;
-         break;
-      case END:
-         // There's only a single gap on the other side to the justification.
-         gap_count = 1;
-         item_gap = 0;
-         start_offset = total_white_space;
-         break;
-      case CENTRE:
-         // We put all items in the centre and then have whitespace either side.
-         gap_count = 2;
-         item_gap = 0;
-         start_offset = total_white_space/2;
-         break;
-      case START_AND_END:
-         // Whitespace is distributed between all items, but none at either end.
-         if(itemCount() == 1) {
-            // With one item : like a START or END case.
+    // For the items, what is the distribution of whitespace?
+    // This depends upon the chosen primary justification style.
+    switch(getPrimaryJustification()) {
+        case START:
+            // There's only a single gap on the other side to the justification.
             gap_count = 1;
-            }
-         else {
-            // With more than one item : one less gap than there are items.
-            gap_count = itemCount() - 1;
-            }
-         item_gap = total_white_space/gap_count;
-         start_offset = 0;
-         break;
-      case SPAN:
-         // Whitespace is distributed between all items, and some at either end.
-         if(itemCount() == 1) {
-            // With one item : like a START or END case.
+            item_gap = 0;
+            start_offset = 0;
+            break;
+        case END:
+            // There's only a single gap on the other side to the justification.
             gap_count = 1;
-            }
-         else {
-            // With more than one item : one more gap than there are items.
-            gap_count = itemCount() + 1;
-            }
-         item_gap = total_white_space/gap_count;
-         start_offset = item_gap/2;
-         break;
-      default:
-         // Shouldn't ever get here!!
-         ErrorHandler::record("HUDFlowLayout::setGaps() - bad switch case reached (default)",
-                              ErrorHandler::FATAL);
-         break;
-      }
-   }
+            item_gap = 0;
+            start_offset = total_white_space;
+            break;
+        case CENTRE:
+            // We put all items in the centre and then have whitespace either side.
+            gap_count = 2;
+            item_gap = 0;
+            start_offset = total_white_space/2;
+            break;
+        case START_AND_END:
+            // Whitespace is distributed between all items, but none at either end.
+            if(itemCount() == 1) {
+                // With one item : like a START case.
+                gap_count = 1;
+                }
+            else {
+                // With more than one item : one less gap than there are items.
+                gap_count = itemCount() - 1;
+                }
+            item_gap = total_white_space/gap_count;
+            start_offset = 0;
+            break;
+        case SPAN:
+            // Whitespace is distributed between all items, and some at either end.
+            if(itemCount() == 1) {
+                // With one item : like a CENTRE case.
+                gap_count = 1;
+                }
+            else {
+                // With more than one item : one more gap than there are items.
+                gap_count = itemCount() + 1;
+                }
+            item_gap = total_white_space/gap_count;
+            start_offset = item_gap/2;
+            break;
+        default:
+            // Shouldn't ever get here!!
+            ErrorHandler::record("HUDFlowLayout::setGaps() - bad switch case reached (default)",
+                                 ErrorHandler::FATAL);
+            break;
+        }
+    }
--- src/solarsystem/HUDFlowLayout.h
+++ src/solarsystem/HUDFlowLayout.h
@@ -30,96 +30,150 @@
  */
 
 /**
- * \brief The container type having a flow layout.
+ * \brief This class enacts a container type having a 'flow' layout.
+ *
+ *      Either a horizontal or vertical axis of flow is chosen upon
+ * construction, which indicates the direction along which to place
+ * any items later added. Thus for a horizontal flow layout any added
+ * items are placed from left to right in the order of their insertion
+ * ie. the most recently added item is placed to the right side of
+ * existing items. Similiarly the vertical flow layout has the items
+ * placed from top to bottom in the order of their insertion ie. the
+ * most recently added item is placed below existing items.
+ *
+ *      For a given direction of flow then there are several choices of
+ * policy to determine the distribution of spacing ( or justification )
+ * between the items :
+ *          - START : places all items to the left side in the case
+ *            of horizontal flow, or the top side in the case of
+ *            vertical flow, with any remaining space placed to
+ *            the right or below respectively.
+ *          - CENTRE : within rounding, the items are placed together
+ *            in the centre of the container, with equal space allotted
+ *            to left and right sides in the case of horizontal flow,
+ *            or equally above and below in the case of vertical flow.
+ *          - END : similiar to START except item placement is to the
+ *            right side for horizontal flow, and at the bottom in the
+ *            case of vertical flow, with any extra space being allocated
+ *            to the left and above respectively.
+ *          - START_AND_END : all extra space is allocated in the gaps
+ *            between items, equally within rounding, and no space at
+ *            either extreme of the container. If only one item present
+ *            then behaves like a START case.
+ *          - SPAN : similiar to START_AND_END except some gap, equal to
+ *            within rounding, at the extreme margins. If only one item
+ *            present then behaves like a CENTRE case.
+ *
+ *      This feature has not yet been implemented. The distribution
+ * of spacing ( or justification ) along the axis orthogonal to the
+ * primary flow has these policies :
+ *          - PROXIMAL : whereby items abut the upper edge of the
+ *            container in the case of horizontal flow, or the left
+ *            edge of the container in the case of vertical flow.
+ *          - MIDDLE : items have, within rounding, equal spacing above
+ *            and below in the case of horizontal flow, or to the left
+ *            and right in the case of vertical flow.
+ *          - DISTAL : similiar to PROXIMAL, but items abut the lower
+ *            container edge for horizontal flow, and the right edge
+ *            for vertical flow.
+ *
+ * \see HUDContainer
+ * \see HUDContent
  *
  * \author Mike Hewson\n
  */
 
 class HUDFlowLayout : public HUDContainer {
-   public:
-      enum primaryJustification {START, CENTRE, END, START_AND_END, SPAN};
+    public:
+        /// Enumerants to specify the mode of justification
+        /// along the primary or 'flow' axis.
+        enum primaryJustification {START, CENTRE, END, START_AND_END, SPAN};
 
-      enum secondaryJustification {PROXIMAL, MIDDLE, DISTAL};
+        /// Enumerants to specify the mode of justification
+        /// along the secondary axis.
+        enum secondaryJustification {PROXIMAL, MIDDLE, DISTAL};
 
-      enum Axis {HORIZONTAL, VERTICAL};
+        /// Enumerant to specify the flow axis.
+        enum Axis {HORIZONTAL, VERTICAL};
 
-      /**
-       * \brief Constructor
-       */
-      HUDFlowLayout(HUDContainer* enclosing, Axis axis);
+        /**
+         * \brief Constructor
+         */
+        HUDFlowLayout(HUDContainer* enclosing, Axis axis);
 
-      /**
-       * \brief Destructor
-       */
-      virtual ~HUDFlowLayout();
+        /**
+         * \brief Destructor
+         */
+        virtual ~HUDFlowLayout();
 
-      /**
-       * \brief Add renderable content to this container.
-       *
-       * \param content : a pointer to the desired content
-       */
-      void addContent(HUDContent* content);
+        /**
+         * \brief Add renderable content to this container.
+         *
+         * \param content : a pointer to the desired content
+         */
+        void addContent(HUDContent* content);
 
-      /**
-       * \brief Set the justification along the primary flow axis.
-       *
-       * \param just : the desired justification
-       */
-      void setPrimaryJustification(primaryJustification just);
+        /**
+         * \brief Set the justification along the primary flow axis.
+         *
+         * \param just : the desired justification
+         */
+        void setPrimaryJustification(primaryJustification just);
 
-      /**
-       * \brief Set the justification along the secondary flow axis.
-       *
-       * \param just : the desired justification
-       */
-      void setSecondaryJustification(secondaryJustification just);
+        /**
+         * \brief Set the justification along the secondary flow axis.
+         *
+         * \param just : the desired justification
+         */
+        void setSecondaryJustification(secondaryJustification just);
 
-      /**
-       * \brief Get the justification of the primary flow axis.
-       *
-       * \return the desired justification
-       */
-      HUDFlowLayout::primaryJustification getPrimaryJustification(void) const;
+        /**
+         * \brief Get the justification of the primary flow axis.
+         *
+         * \return the desired justification
+         */
+        HUDFlowLayout::primaryJustification getPrimaryJustification(void) const;
 
-      /**
-       * \brief Get the justification of the secondary flow axis.
-       *
-       * \return the desired justification
-       */
-      HUDFlowLayout::secondaryJustification getSecondaryJustification(void) const;
+        /**
+         * \brief Get the justification of the secondary flow axis.
+         *
+         * \return the desired justification
+         */
+        HUDFlowLayout::secondaryJustification getSecondaryJustification(void) const;
 
-   protected:
-      /**
-       * \brief Determine the miminum dimensions as per the internal
-       *        layout of items within. Enacts HUDContainer interface.
-       *
-       * \return std::pair composed of ( in order )
-       *          - the desired width
-       *          - the desired height
-       */
-      virtual std::pair<GLuint, GLuint> reassessMinimumDimensions(void);
+    protected:
+        /**
+         * \brief Determine the miminum dimensions as per the internal
+         *        layout of items within. Enacts HUDContainer interface.
+         *
+         * \return std::pair composed of ( in order )
+         *          - the desired width
+         *          - the desired height
+         */
+        virtual std::pair<GLuint, GLuint> reassessMinimumDimensions(void);
 
-      /**
-       * \brief Determine and set the base positions of each item.
-       *        Enacts HUDContainer interface.
-       */
-      virtual void allocateItemBases(void);
+        /**
+         * \brief Determine and set the base positions of each item.
+         *        Enacts HUDContainer interface.
+         */
+        virtual void allocateItemBases(void);
 
-   private:
-      /* brief Determine how many of, and how wide, whitespace gaps as
-       *       per the justification setting.
-       */
-      void setGaps(void);
+    private:
+        /** brief Determine how many of, and how wide are, the
+        *         whitespace gaps as per the justification setting.
+         */
+        void setGaps(void);
 
-      GLuint gap_count;
-      GLuint total_white_space;
-      GLuint start_offset;
-      GLuint item_gap;
+        GLuint gap_count;
+        GLuint total_white_space;
+        GLuint start_offset;
+        GLuint item_gap;
 
-      Axis ax;
-      primaryJustification primary_just;
-      secondaryJustification secondary_just;
-   };
+        /// The axis and justification settings.
+        Axis ax;
+        primaryJustification primary_just;
+        secondaryJustification secondary_just;
+    };
 
 /**
  * @}
