14a0dc14814506ec86f5c99f22f019fb3a297f33
Mike Hewson <hewsmike@iinet.net.au>
2012-07-19 11:25:09 +1000

    Events class prettification

--- src/framework/Events.cpp
+++ src/framework/Events.cpp
@@ -24,7 +24,8 @@
 
 #include "ErrorHandler.h"
 
-// Use minimum timing intervals in milliseconds for BOINC and render callbacks.
+// Use minimum timing intervals in milliseconds for BOINC
+// and render callbacks.
 GLuint Events::BOINC_CALLBACK_INTERVAL(1000);
 GLuint Events::RENDER_CALLBACK_INTERVAL_MIN(TriggerTimer::TRIGGER_MIN);
 
@@ -35,216 +36,218 @@ std::list<Event> Events::event_queue;
 std::auto_ptr<Events> Events::p_instance;
 
 Events::Events(void) {
-	}
+    }
 
 Events::Events(const Events& other) {
-	}
+    }
 
 Events& Events::operator=(const Events& other) {
-	// Skip self assignment check as no allocation.
-	return *this;
-	}
+    // Skip self assignment check as no allocation.
+    return *this;
+    }
 
 Events* Events::Instance(GLuint render_interval) {
-	// If we have no instance, then make one and initialise it.
-	if(Events::p_instance.get() == NULL) {
-		Events::p_instance = std::auto_ptr<Events>(new Events());
-		init(render_interval);
-		}
+    // If we have no instance, then make one and initialise it.
+    if(Events::p_instance.get() == NULL) {
+        Events::p_instance = std::auto_ptr<Events>(new Events());
+        init(render_interval);
+        }
 
-	return Events::p_instance.get();
-	}
+    return Events::p_instance.get();
+    }
 
 void Events::init(GLuint render_interval) {
-	// Empty the event queue.
-	event_queue.clear();
+    // Empty the event queue.
+    event_queue.clear();
 
-	// Set GLFW global event behaviours.
+    // Set GLFW global event behaviours.
 
-	// Ensure keys are non-sticky.
-	glfwDisable(GLFW_STICKY_KEYS);
+    // Ensure keys are non-sticky.
+    glfwDisable(GLFW_STICKY_KEYS);
 
-	// Ensure keys do not repeat if held down.
-	glfwDisable(GLFW_KEY_REPEAT);
+    // Ensure keys do not repeat if held down.
+    glfwDisable(GLFW_KEY_REPEAT);
 
-	// Ensure system key-strokes remain active.
-	glfwEnable(GLFW_SYSTEM_KEYS);
+    // Ensure system key-strokes remain active.
+    glfwEnable(GLFW_SYSTEM_KEYS);
 
-	// Ensure mouse buttons are non-sticky.
-	// Actually, I didn't know you could have sticky ones ... :-)
-	glfwDisable(GLFW_STICKY_MOUSE_BUTTONS);
+    // Ensure mouse buttons are non-sticky.
+    // Actually, I didn't know you could have sticky ones ... :-)
+    glfwDisable(GLFW_STICKY_MOUSE_BUTTONS);
 
-	// Non GLFW callbacks. See note below.
+    // Non GLFW callbacks. See note below.
 
-	// Set the BOINC callback timing.
-	boinc_timer = new TriggerTimer(BOINC_CALLBACK_INTERVAL, boincUpdate);
+    // Set the BOINC callback timing.
+    boinc_timer = new TriggerTimer(BOINC_CALLBACK_INTERVAL, boincUpdate);
 
-	// Set the render callback timing, but adhere to minimum.
-	if(render_interval < RENDER_CALLBACK_INTERVAL_MIN){
-		render_interval = RENDER_CALLBACK_INTERVAL_MIN;
-		}
-	render_timer = new TriggerTimer(render_interval, render);
+    // Set the render callback timing, but adhere to minimum.
+    if(render_interval < RENDER_CALLBACK_INTERVAL_MIN){
+        render_interval = RENDER_CALLBACK_INTERVAL_MIN;
+        }
+    render_timer = new TriggerTimer(render_interval, render);
 
-	// Those GLFW callbacks below herewith defined are to 'ordinary' functions ie. not
-	// part of any C++ class definition - not even static. The reason is that GLFW is
-	// a C library, hence has no 'this' pointer and other OOP stuff, etc. The BOINC
-	// and render callbacks mechanisms are of my own making, not GLFW library
-	// linked and so such concerns are not relevant. An issue of language
-	// calling conventions ultimately.
+    // These GLFW callbacks below herewith defined are to 'ordinary'
+    // functions ie. not part of any C++ class definition - not even
+    // static. The reason is that GLFW is a C library, hence has no
+    // 'this' pointer and other OOP stuff, etc. The BOINC and render
+    // callbacks mechanisms are of my own making, not GLFW library
+    // linked and so such concerns are not relevant. An issue of
+    // language calling conventions ultimately.
 
-	// Set the keypress callback.
-	glfwSetKeyCallback(keyPress);
+    // Set the keypress callback.
+    glfwSetKeyCallback(keyPress);
 
-	// Set the character input callback.
-	glfwSetCharCallback(charInput);
+    // Set the character input callback.
+    glfwSetCharCallback(charInput);
 
-	// Set the mouse button callback.
-	glfwSetMouseButtonCallback(mouseButton);
+    // Set the mouse button callback.
+    glfwSetMouseButtonCallback(mouseButton);
 
-	// Set the mouse movement callback.
-	glfwSetMousePosCallback(mouseMotion);
+    // Set the mouse movement callback.
+    glfwSetMousePosCallback(mouseMotion);
 
-	// Set the mouse wheel callback.
-	glfwSetMouseWheelCallback(mouseWheel);
+    // Set the mouse wheel callback.
+    glfwSetMouseWheelCallback(mouseWheel);
 
-	// Set the window resize callback.
-	glfwSetWindowSizeCallback(resize);
+    // Set the window resize callback.
+    glfwSetWindowSizeCallback(resize);
 
-	// Set the window quit callback.
-	glfwSetWindowCloseCallback(quit);
+    // Set the window quit callback.
+    glfwSetWindowCloseCallback(quit);
 
-	// Finally GLFW will poll devices for events with every buffer swap.
-	glfwEnable(GLFW_AUTO_POLL_EVENTS);
-	}
+    // Finally GLFW will poll devices for events with every buffer swap.
+    glfwEnable(GLFW_AUTO_POLL_EVENTS);
+    }
 
 bool Events::next(Event* ev) {
-   // Assume failure.
-   bool ret_val = false;
+    // Assume failure.
+    bool ret_val = false;
 
-   // Assuming a non-NULL pointer has been passed.
-   if(ev != NULL) {
-      // If there is some event available.
-      if(!event_queue.empty()) {
-         // Copy the event to the calling routine.
-         *ev = event_queue.front();
+    // Assuming a non-NULL pointer has been passed.
+    if(ev != NULL) {
+        // If there is some event available.
+        if(!event_queue.empty()) {
+            // Copy the event to the calling routine.
+            *ev = event_queue.front();
 
-         // Remove this event from the queue.
-         event_queue.pop_front();
+            // Remove this event from the queue.
+            event_queue.pop_front();
 
-         // Signal success.
-         ret_val = true;
-         }
-      }
+            // Signal success.
+            ret_val = true;
+            }
+        }
 
-   return ret_val;
-   }
+    return ret_val;
+    }
 
 void Events::tick(void) {
-	boinc_timer->update();
-   render_timer->update();
-   glfwPollEvents();
-	}
+    boinc_timer->update();
+    render_timer->update();
+    glfwPollEvents();
+    }
 
 void Events::flush(void) {
-	// Just toss 'em without ceremony.
-	event_queue.clear();
-	}
+    // Just toss 'em without ceremony.
+    event_queue.clear();
+    }
 
 void Events::boincUpdate(void) {
-	Event ev;
-   ev.b_update.type = BOINCUpdateEventType;
+    Event ev;
+    ev.b_update.type = BOINCUpdateEventType;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void Events::render(void) {
-   Event ev;
-   ev.render.type = RenderEventType;
+    Event ev;
+    ev.render.type = RenderEventType;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::charInput(int character, int action) {
-	Event ev;
-   ev.c_input.type = Events::CharInputEventType;
-   ev.c_input.char_code = character;
+    Event ev;
+    ev.c_input.type = Events::CharInputEventType;
+    ev.c_input.char_code = character;
 
-   // Default is key release.
-   ev.c_input.pressed = false;
-   if(action == GLFW_PRESS){
-      // Otherwise key was pressed.
-      ev.c_input.pressed = true;
-      }
+    // Default is key release.
+    ev.c_input.pressed = false;
+    if(action == GLFW_PRESS){
+        // Otherwise key was pressed.
+        ev.c_input.pressed = true;
+        }
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::keyPress(int key, int action) {
-	Event ev;
-   ev.k_press.type = Events::KeyPressEventType;
-   ev.k_press.key_code = key;
+    Event ev;
+    ev.k_press.type = Events::KeyPressEventType;
+    ev.k_press.key_code = key;
 
-   // Default is key release.
-   ev.k_press.pressed = false;
-   if(action == GLFW_PRESS){
-      // Otherwise key was pressed.
-      ev.k_press.pressed = true;
-      }
+    // Default is key release.
+    ev.k_press.pressed = false;
+    if(action == GLFW_PRESS){
+        // Otherwise key was pressed.
+        ev.k_press.pressed = true;
+        }
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::mouseButton(int button, int action) {
-	std::stringstream msg;
-	msg << "Events::mouseButton() : button_code = "
-		 << button
-		 << " \taction = "
-		 << action;
-	ErrorHandler::record(msg.str(), ErrorHandler::INFORM);
+    std::stringstream msg;
+    msg << "Events::mouseButton() : button_code = "
+        << button
+        << " \taction = "
+        << action;
+    ErrorHandler::record(msg.str(), ErrorHandler::INFORM);
 
-   Event ev;
-   ev.m_button.type = Events::MouseButtonEventType;
+    Event ev;
+    ev.m_button.type = Events::MouseButtonEventType;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::mouseMotion(int x, int y) {
-	static int last_x = 0;
-	static int last_y = 0;
+    static int last_x = 0;
+    static int last_y = 0;
 
-	Event ev;
-   ev.m_motion.type = Events::MouseMotionEventType;
-   ev.m_motion.xrel = x - last_x;
-   ev.m_motion.yrel = y - last_y;
+    Event ev;
+    ev.m_motion.type = Events::MouseMotionEventType;
+    ev.m_motion.xrel = x - last_x;
+    ev.m_motion.yrel = y - last_y;
 
-   last_x = x;
-   last_y = y;
+    last_x = x;
+    last_y = y;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::mouseWheel(int pos) {
-	Event ev;
-   ev.m_wheel.type = Events::MouseWheelEventType;
+    Event ev;
+    ev.m_wheel.type = Events::MouseWheelEventType;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 void GLFWCALL Events::resize(int width, int height) {
-	Event ev;
-   ev.resize.type = Events::ResizeEventType;
-   ev.resize.width = width;
-   ev.resize.height = height;
+    Event ev;
+    ev.resize.type = Events::ResizeEventType;
+    ev.resize.width = width;
+    ev.resize.height = height;
 
-   event_queue.push_back(ev);
-   }
+    event_queue.push_back(ev);
+    }
 
 int GLFWCALL Events::quit(void) {
-	Event ev;
-   ev.quit.type = Events::QuitEventType;
+    Event ev;
+    ev.quit.type = Events::QuitEventType;
 
-   event_queue.push_back(ev);
+    event_queue.push_back(ev);
 
-   // Allow the window closure to continue ( GL_FALSE would prevent that )
-   return GL_TRUE;
-   }
+    // Allow the window closure to continue
+    // ( GL_FALSE here would prevent that )
+    return GL_TRUE;
+    }
--- src/framework/Events.h
+++ src/framework/Events.h
@@ -31,63 +31,63 @@
 // The various events to manage, with their individual parameters.
 
 struct BOINCUpdateEvent {
-   GLuint type;
-   };
+    GLuint type;
+    };
 
 struct CharInputEvent {
-   GLuint type;
-   int char_code;
-   bool pressed;
-   };
+    GLuint type;
+    int char_code;
+    bool pressed;
+    };
 
 struct KeyPressEvent {
-   GLuint type;
-   int key_code;
-   bool pressed;
-   };
+    GLuint type;
+    int key_code;
+    bool pressed;
+    };
 
 struct MouseButtonEvent {
-   GLuint type;
-   int button;
-   };
+    GLuint type;
+    int button;
+    };
 
 struct MouseMotionEvent {
-   GLuint type;
-   int xrel;
-   int yrel;
-   };
+    GLuint type;
+    int xrel;
+    int yrel;
+    };
 
 struct MouseWheelEvent {
-   GLuint type;
-   };
+    GLuint type;
+    };
 
 struct RenderEvent {
-   GLuint type;
-   };
+    GLuint type;
+    };
 
 struct ResizeEvent {
-   GLuint type;
-   GLuint width;
-   GLuint height;
-   };
+    GLuint type;
+    GLuint width;
+    GLuint height;
+    };
 
 struct QuitEvent {
-   GLuint type;
-   };
+    GLuint type;
+    };
 
 // Unite the events.
 union Event {
-   GLuint type;
-   BOINCUpdateEvent b_update;
-   CharInputEvent c_input;
-   KeyPressEvent k_press;
-   MouseMotionEvent m_motion;
-   MouseButtonEvent m_button;
-   MouseWheelEvent m_wheel;
-   RenderEvent render;
-   ResizeEvent resize;
-   QuitEvent quit;
-   };
+    GLuint type;
+    BOINCUpdateEvent b_update;
+    CharInputEvent c_input;
+    KeyPressEvent k_press;
+    MouseMotionEvent m_motion;
+    MouseButtonEvent m_button;
+    MouseWheelEvent m_wheel;
+    RenderEvent render;
+    ResizeEvent resize;
+    QuitEvent quit;
+    };
 
 /**
  * \addtogroup framework Framework
@@ -98,7 +98,7 @@ union Event {
  * \brief This class is responsible for event capture and management
  *        of an event queue, currently utilising GLFW.
  *
- *	Based upon the design of Mr Paul Whitehead :
+ *  Based upon the design of Mr Paul Whitehead :
  *
  *     A Leak-Free Singleton Class
  *     http://www.codeguru.com/cpp/cpp/cpp_mfc/singletons/article.php/c755/A-LeakFree-Singleton-Class.htm
@@ -114,148 +114,148 @@ union Event {
  */
 
 class Events {
-	public:
-		enum eventType {BOINCUpdateEventType,
-							 CharInputEventType,
-							 KeyPressEventType,
-							 MouseMotionEventType,
-							 MouseButtonEventType,
-							 MouseWheelEventType,
-							 RenderEventType,
-							 ResizeEventType,
-							 QuitEventType};
+    public:
+        enum eventType {BOINCUpdateEventType,
+                        CharInputEventType,
+                        KeyPressEventType,
+                        MouseMotionEventType,
+                        MouseButtonEventType,
+                        MouseWheelEventType,
+                        RenderEventType,
+                        ResizeEventType,
+                        QuitEventType};
 
-		enum buttonType {LEFT_BUTTON,
-							  MIDDLE_BUTTON,
-							  RIGHT_BUTTON};
+        enum buttonType {LEFT_BUTTON,
+                         MIDDLE_BUTTON,
+                         RIGHT_BUTTON};
 
-		/**
-       * \brief Retrieve ( maybe create ) the singleton, possibly initialising
-		 *	event management.
-       *
-       * \param render_interval milliseconds between render calls,
-       *                        only relevant for the first call to
-       *                        this routine.
-       */
-		static Events* Instance(GLuint render_interval);
+        /**
+         * \brief Retrieve ( maybe create ) the singleton, possibly
+         *        initialising event management.
+         *
+         * \param render_interval : number of milliseconds between
+         *                          render calls, only relevant for
+         *                          the first call to this routine.
+         */
+        static Events* Instance(GLuint render_interval);
 
-		/**
-       * \brief Get the next event, if any, from the queue ( non-blocking ).
-       *
-       * \param ev pointer to an Event which may be filled with
-       *              the detail of said event.
-       * \return boolean indicating :
-       * 					 true, if an event was available, and the queue
-       *                 	will have one fewer.
-       *                 false, if no event was waiting to be read.
-       */
-		static bool next(Event* ev);
+        /**
+         * \brief Get the next event, if any, from the queue ( non-blocking ).
+         *
+         * \param ev  : pointer to an Event which may be filled with
+         *              the detail of said event.
+         * \return boolean indicating :
+         *                  true, if an event was available, and the queue
+         *                        will have one fewer.
+         *                  false, if no event was waiting to be read.
+         */
+        static bool next(Event* ev);
 
-		/**
-       * \brief Keep our special timers ticking over.
-       */
-		static void tick(void);
+        /**
+         * \brief Keep our special timers ticking over.
+         */
+        static void tick(void);
 
-		/**
-		 * \brief Discard all events currently in the queue.
-		 */
-		static void flush(void);
+        /**
+         * \brief Discard all events currently in the queue.
+         */
+        static void flush(void);
 
-   private:
-      /// Prevent direct construction, copying and assignment.
-		Events(void);
-		Events(const Events& other);
-		Events& operator=(const Events& other);
+    private:
+        /// Prevent direct construction, copying and assignment.
+        Events(void);
+        Events(const Events& other);
+        Events& operator=(const Events& other);
 
-		/// Pointer to any single instance.
-		static std::auto_ptr<Events> p_instance;
+        /// Pointer to any single instance.
+        static std::auto_ptr<Events> p_instance;
 
-		/// Intervals in milliseconds for our special timers.
-		static GLuint BOINC_CALLBACK_INTERVAL;
-		static GLuint RENDER_CALLBACK_INTERVAL_MIN;
+        /// Intervals in milliseconds for our special timers.
+        static GLuint BOINC_CALLBACK_INTERVAL;
+        static GLuint RENDER_CALLBACK_INTERVAL_MIN;
 
-      /**
-       * \brief Initialise this event manager.
-       *
-       * \param render_interval pointer to an Event which may be filled with
-       *                        the detail of said event.
-       */
-		static void init(GLuint render_interval);
+        /**
+         * \brief Initialise this event manager.
+         *
+         * \param render_interval : desired number of milliseconds
+         *                          between render calls.
+         */
+        static void init(GLuint render_interval);
 
-		/**
-       * \brief Callback for a BOINC update event.
-       */
-		static void boincUpdate(void);
+        /**
+         * \brief Callback for a BOINC update event.
+         */
+        static void boincUpdate(void);
 
-		/**
-       * \brief Callback for a render event.
-       */
-		static void render(void);
+        /**
+         * \brief Callback for a render event.
+         */
+        static void render(void);
 
-		/**
-       * \brief Callback for a character input event.
-       *
-       * \param character - from Unicode ( ISO 10646 ).
-       * \param action - one of GLFW_PRESS or GLFW_RELEASE.
-       */
-		static void GLFWCALL charInput(int character, int action);
+        /**
+         * \brief Callback for a character input event.
+         *
+         * \param character : from Unicode ( ISO 10646 ).
+         * \param action : one of GLFW_PRESS or GLFW_RELEASE.
+         */
+        static void GLFWCALL charInput(int character, int action);
 
-		/**
-       * \brief Callback for a key event.
-       *
-       * \param character - uppercase printable ISO 8859-1 character
-		 *							 or a special key identifier per GLFW.
-       * \param action - one of GLFW_PRESS or GLFW_RELEASE.
-       */
-		static void GLFWCALL keyPress(int key, int action);
+        /**
+         * \brief Callback for a key event.
+         *
+         * \param character : uppercase printable ISO 8859-1 character
+         *                    a special key identifier per GLFW.
+         * \param action : one of GLFW_PRESS or GLFW_RELEASE.
+         */
+        static void GLFWCALL keyPress(int key, int action);
 
-		/**
-       * \brief Callback for a mouse button event.
-       *
-       * \param button - one of the GLFW mouse button identifiers.
-       * \param action - one of GLFW_PRESS or GLFW_RELEASE.
-       */
-		static void GLFWCALL mouseButton(int button, int action);
+        /**
+         * \brief Callback for a mouse button event.
+         *
+         * \param button - one of the GLFW mouse button identifiers.
+         * \param action - one of GLFW_PRESS or GLFW_RELEASE.
+         */
+        static void GLFWCALL mouseButton(int button, int action);
 
-		/**
-       * \brief Callback for a mouse motion event.
-       *
-       * \param x - mouse position in the 'horizontal' axis.
-       * \param y - mouse position in the 'vertical' axis.
-       */
-		static void GLFWCALL mouseMotion(int x, int y);
+        /**
+         * \brief Callback for a mouse motion event.
+         *
+         * \param x - mouse position in the 'horizontal' axis.
+         * \param y - mouse position in the 'vertical' axis.
+         */
+        static void GLFWCALL mouseMotion(int x, int y);
 
-		/**
-       * \brief Callback for a mousewheel event.
-       *
-       * \param pos - mouse wheel position.
-       */
-		static void GLFWCALL mouseWheel(int pos);
+        /**
+         * \brief Callback for a mousewheel event.
+         *
+         * \param pos - mouse wheel position.
+         */
+        static void GLFWCALL mouseWheel(int pos);
 
-		/**
-       * \brief Callback for a window resize event.
-       *
-       * \param width - the new width in the 'horizontal' axis.
-       * \param height - the new width in the 'vertical' axis.
-       */
-		static void GLFWCALL resize(int width, int height);
+        /**
+         * \brief Callback for a window resize event.
+         *
+         * \param width - the new width in the 'horizontal' axis.
+         * \param height - the new width in the 'vertical' axis.
+         */
+        static void GLFWCALL resize(int width, int height);
 
-		/**
-       * \brief Callback for a window close event.
-       *
-       * \return - whether or not closure ought proceed, GL_TRUE or GL_FALSE
-       */
-		static int GLFWCALL quit(void);
+        /**
+         * \brief Callback for a window close event.
+         *
+         * \return - whether or not closure ought proceed, GL_TRUE or GL_FALSE
+         */
+        static int GLFWCALL quit(void);
 
-		/// A timer for BOINC updates.
-		static TriggerTimer* boinc_timer;
+        /// A timer for BOINC updates.
+        static TriggerTimer* boinc_timer;
 
-		/// A timer for rendering a frame.
-		static TriggerTimer* render_timer;
+        /// A timer for rendering a frame.
+        static TriggerTimer* render_timer;
 
-		/// The event queue with double ended semantics.
-		static std::list<Event> event_queue;
-   };
+        /// The event queue with double ended semantics.
+        static std::list<Event> event_queue;
+    };
 
 /**
  * @}
