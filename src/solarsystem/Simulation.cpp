af3bb3c0ad5717cff3b1b47e2e61169e12100ab3
Mike Hewson <hewsmike@iinet.net.au>
2012-10-12 11:31:47 +1100

    Interim

--- src/solarsystem/Simulation.cpp
+++ src/solarsystem/Simulation.cpp
@@ -229,7 +229,7 @@ void Simulation::step(void) {
             // Then put new images, if any, into the panel.
             // Derived according to the current position in the tour.
             // Cast from base ( Renderable ) to derived ( HUDContent ) class.
-            const static_cast<std::vector<HUDContent*> >& images = pilot.getImages().pointers();
+            static_cast<std::vector<HUDContent*> >(images) = pilot.getImages().pointers();
             for(unsigned int im = 0; im < images.size(); ++im) {
                 west_panel.addContent(images[im]);
                 }
