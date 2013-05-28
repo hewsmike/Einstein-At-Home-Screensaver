4f51a6aedddf21f3825057866e06bfdc60aa8008
Mike Hewson <hewsmike@iinet.net.au>
2013-05-28 15:53:28 +1000

    Updated main.cpp for Starsphere

--- src/starsphere/main.cpp
+++ src/starsphere/main.cpp
@@ -27,10 +27,12 @@
 
 #include "../erp_git_version.h"
 
-#include "WindowManager.h"
-#include "ResourceFactory.h"
 #include "AbstractGraphicsEngine.h"
+#include "ErrorHandler.h"
+#include "Events.h"
 #include "GraphicsEngineFactory.h"
+#include "ResourceFactory.h"
+#include "WindowManager.h"
 
 #ifdef __APPLE__
 #include "EaHMacIcon.h"
@@ -43,10 +45,47 @@ extern "C" {
 #endif
 #endif
 
+void openGLReport(WindowManager* window) {
+    // Discover and record the OpenGL version.
+    GLuint major = 0;
+    GLuint minor = 0;
+    window->getOGLVersion(&major, &minor);
+    std::stringstream msg1;
+    msg1 << "SolarSystem::main() : OpenGL version = "
+         << major << '.' << minor;
+    ErrorHandler::record(msg1.str(), ErrorHandler::INFORM);
 
-int main(int argc, char **argv)
-{
-    // print version info
+    // Discover and record the OpenGL vendor.
+    const GLubyte* vendor = glGetString(GL_VENDOR);
+    if(vendor != NULL) {
+        std::stringstream msg2;
+        msg2 << "SolarSystem::main() : OpenGL vendor string = "
+             << "'"
+             << ErrorHandler::convertGLstring(vendor)
+             << "'";
+        ErrorHandler::record(msg2.str(), ErrorHandler::INFORM);
+        }
+    else {
+        ErrorHandler::record("SolarSystem::main() : I got a null for GL_VENDOR", ErrorHandler::WARN);
+        }
+
+    // Discover and record the OpenGL renderer.
+    const GLubyte* renderer = glGetString(GL_RENDERER);
+    if(renderer != NULL) {
+        std::stringstream msg3;
+        msg3 << "SolarSystem::main() : OpenGL renderer string = "
+             << "'"
+             << ErrorHandler::convertGLstring(renderer)
+             << "'";
+        ErrorHandler::record(msg3.str(), ErrorHandler::INFORM);
+        }
+    else {
+        ErrorHandler::record("SolarSystem::main() : I got a null for GL_RENDERER", ErrorHandler::WARN);
+        }
+    }
+
+int main(int argc, char **argv) {
+    // Print version info if the command line requests it.
     if(argc == 2) {
         string param(argv[1]);
         if(param == "--version" || param == "-v") {
@@ -54,101 +93,119 @@ int main(int argc, char **argv)
             cout << "Graphics Application Revision: " << ERP_GIT_VERSION << endl;
             cout << "BOINC Revision: " << SVN_VERSION << endl;
             exit(0);
+            }
         }
-    }
 
-    // enable BOINC diagnostics
-	// TODO: we might want to optimize this for glibc- and mingw-based stacktraces!
-	boinc_init_graphics_diagnostics(BOINC_DIAG_DEFAULTS);
+    // Enable BOINC diagnostics
+    /// TODO: we might want to optimize this for glibc- and mingw-based stacktraces!
+    boinc_init_graphics_diagnostics(BOINC_DIAG_DEFAULTS);
 
 #ifdef __APPLE__
-  setMacIcon(argv[0], MacAppIconData, sizeof(MacAppIconData));
+    // Supply an icon for Mac's.
+    setMacIcon(argv[0], MacAppIconData, sizeof(MacAppIconData));
 #endif
 
-	// choose application to be build/used
-	GraphicsEngineFactory::Applications scienceApplication;
+    // Choose application type to be built/used.
+    GraphicsEngineFactory::Applications scienceApplication;
 #ifdef SCIENCE_APP
-	scienceApplication = GraphicsEngineFactory::SCIENCE_APP;
+    // The SCIENCE_APP symbol is defined in the makefile
+    // that produces the build for this product.
+    scienceApplication = GraphicsEngineFactory::SCIENCE_APP;
 #else
-	scienceApplication = GraphicsEngineFactory::EinsteinS5R3;
+    // And if SCIENCE_APP was not given, this is the default.
+    scienceApplication = GraphicsEngineFactory::EinsteinGravity;
 #endif
 
-	// prepare main objects
-	WindowManager window;
-	ResourceFactory factory;
-	AbstractGraphicsEngine *graphics = GraphicsEngineFactory::createInstance(
-											GraphicsEngineFactory::Starsphere,
-											scienceApplication);
+    // Instantiate and initialize our window manager.
+    WindowManager::displaymode mode = WindowManager::WINDOW;
 
-	if(!graphics) {
-		cerr << "Requested graphics engine could not be found/instantiated!" << endl;
-		exit(1);
-	}
+    // Check other optional command line parameters
+    if(argc == 2) {
+        string param(argv[1]);
+        if(param == "--fullscreen") {
+            // Set non-interactive mode ie. 'screensaver'
+            // (must do this first on Apple).
+            mode = WindowManager::SCREENSAVER;
+            }
+        if(param == "--demo") {
+            mode = WindowManager::DEMO;
+            }
+        }
 
-    // initialize window manager
-    if(!window.initialize()) {
-    	cerr << "Window manager could not be initialized!" << endl;
-    	delete graphics;
-        exit(1);
-    }
+    WindowManager window(mode);
+#ifdef __APPLE__
+            SetMacSSLevel();
+#endif
 
-	// create font and icon resource instances
-	const Resource* fontResource = factory.createInstance("FontSansSerif");
-	const Resource* iconResource = factory.createInstance("AppIconBMP");
+    // Make an AbstractGraphicsEngine on the heap, FATAL on fail.
+    // Edit this call for different build sources ie. swap 'Solarsystem' for whatever
+    AbstractGraphicsEngine* graphics = GraphicsEngineFactory::createInstance(GraphicsEngineFactory::Starsphere,
+                                                                             scienceApplication);
+    if(!graphics) {
+        ErrorHandler::record("SolarSystem::main() : Requested graphics engine could not be found/instantiated!", ErrorHandler::FATAL);
+        }
 
-	if(fontResource == NULL) {
-		cerr << "Font resource could not be loaded!" << endl;
-		delete graphics;
-		exit(1);
-	}
+    // Attempt to initialise our WindowManager,
+    // if succeeds then will be in windowed mode.
+    if(window.initialize() != true) {
+        // Failure, so destroy the AbstractGraphicsEngine and ...
+        delete graphics;
+        // ... emit a message with exit.
+        ErrorHandler::record("SolarSystem::main() : Window manager could not be initialized!", ErrorHandler::FATAL);
+        }
 
-	if(fontResource->data()->size() <= 0) {
-		cerr << "Font resource could not be loaded!" << endl;
-		delete graphics;
-		delete fontResource;
-		exit(1);
-	}
+    // Collate and record OpenGL info.
+    openGLReport(&window);
 
-	if(iconResource != NULL && iconResource->data()->size() > 0) {
-		window.setWindowIcon(&iconResource->data()->at(0), iconResource->data()->size());
-		delete iconResource;
-	}
-	else {
-		cerr << "Icon resource could not be loaded! Continuing anyway..." << endl;
-	}
+    // Using a ResourceFactory, create font and icon resource instances
+    ResourceFactory factory;
+    const Resource* fontResource = factory.createInstance("FontSansSerif");
+    const Resource* iconResource = factory.createInstance("AppIconBMP");
 
+    // Check if font resource is available.
+    if(fontResource == NULL) {
+        // No, so that's a fatal. Exit with appropriate deletions and message.
+        delete graphics;
+        ErrorHandler::record("SolarSystem::main() : Font resource is NULL!", ErrorHandler::FATAL);
+        }
+
+    // Given that you have a font resource, is the size meaningful?
+    if(fontResource->data()->size() <= 0) {
+        // No, so that's also a fatal. Exit with appropriate deletions and message.
+        delete graphics;
+        delete fontResource;
+        ErrorHandler::record("SolarSystem::main() : Font resource could not be loaded!", ErrorHandler::FATAL);
+        }
+
+    // Check for an icon resource, but this is not fatal in the lack.
+    if(iconResource != NULL && iconResource->data()->size() > 0) {
+        window.setWindowIcon(&iconResource->data()->at(0), iconResource->data()->size());
+        delete iconResource;
+        }
+    else {
+        ErrorHandler::record("SolarSystem::main() : Icon resource could not be loaded! Continuing anyway...", ErrorHandler::WARN);
+        }
+
+    // Set the caption or window title.
+    /// TODO - if/when GLFW 3.x arrives, put in a window icon as well ?
     window.setWindowCaption("Einstein@Home");
 
-    // register starsphere as event observer
+    // Prepare for rendering by initialising chosen engine
+    // and get up to date BOINC information.
+    graphics->initialize(window.windowWidth(), window.windowHeight(), fontResource);
+    graphics->refreshBOINCInformation();
+
+    // Register AbstractGraphicsEngine as event observer.
     window.registerEventObserver(graphics);
 
-	// pepare rendering
-	graphics->initialize(window.windowWidth(), window.windowHeight(), fontResource);
-	graphics->refreshBOINCInformation();
+    // Enter main event loop, but first flush any pending events.
+    Events::Instance(0)->flush();
+    window.eventLoop();
 
-	// check optional command line parameters
-	if(argc == 2) {
-		string param(argv[1]);
-		if(param == "--fullscreen") {
-			// set non-interactive mode (must do this first on Apple)
-			window.setScreensaverMode(true);
-		}
-		if(param == "--fullscreen" || param == "--demo") {
-			// switch to fullscreen (on windoze: after init!)
-			window.toggleFullscreen();
-#ifdef __APPLE__
-			SetMacSSLevel();
-#endif
-		}
-	}
+    // Clean up and exit
+    window.unregisterEventObserver(graphics);
+    delete graphics;
+    delete fontResource;
 
-	// enter main event loop
-	window.eventLoop();
-
-	// clean up end exit
-	window.unregisterEventObserver(graphics);
-	delete graphics;
-	delete fontResource;
-
-	exit(0);
-}
+    exit(0);
+    }
