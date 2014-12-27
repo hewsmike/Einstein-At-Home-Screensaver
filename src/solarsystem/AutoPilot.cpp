0ceb3eebf01f533144e2625dc4885a5bdd0b98f3
Mike Hewson <hewsmike@iinet.net.au>
2014-12-28 10:34:40 +1100

    Revert "Version 1.00"
    This reverts commit 87be229d42525b3be44992a3d4283c3887e28721.
    
    Conflicts:
    	src/solarsystem/AutoPilot.cpp

--- src/solarsystem/AutoPilot.cpp
+++ src/solarsystem/AutoPilot.cpp
@@ -23,6 +23,7 @@
 #include <cmath>
 
 #include "ErrorHandler.h"
+#include "LookOut.h"
 
 const float AutoPilot::LENGTH_PER_FRAME(15.0f);
 const float AutoPilot::LEAST_PATH_LENGTH(0.00001f);
@@ -229,6 +230,7 @@ void AutoPilot::getTraverse(const Traversable& trav, const CameraState& cam) {
     // Construct a Lookout from the existing position and orientation
     // within the Simulation.
     LookOut first(cam.position(), cam.focus(), cam.orientation());
+    current_traverse.addLookout(first);
 
     // The set of Lookouts come from alternating our initial
     // Lookout with the given Traversable object.
@@ -236,10 +238,20 @@ void AutoPilot::getTraverse(const Traversable& trav, const CameraState& cam) {
         way_point < trav.numberOfWayPoints();
         ++way_point) {
 <<<<<<< HEAD
+<<<<<<< HEAD
         current_traverse.addLookout(first);
 =======
         // current_traverse.addLookout(first);
 >>>>>>> parent of 2690d47... Interim
         current_traverse.addLookout(trav.getView(way_point));
+=======
+        // current_traverse.addLookout(first);
+        LookOut temp = trav.getView(way_point);
+        current_traverse.addLookout(temp);
+        const Vector3D pos(temp.position());
+        const Vector3D foc(0, 0, 0);
+        const Vector3D ori(temp.orientation());
+        current_traverse.addLookout(LookOut(pos, foc, ori));
+>>>>>>> parent of 87be229... Version 1.00
         }
     }
