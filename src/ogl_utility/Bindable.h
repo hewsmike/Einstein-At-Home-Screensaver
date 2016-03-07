c0f9a2c60ecef865ccb9a2ab1dcdc95bbacfe6dc
Mike Hewson <hewsmike@iinet.net.au>
2016-03-07 16:19:44 +1100

    New Bindable Interface

--- src/ogl_utility/Bindable.h
+++ src/ogl_utility/Bindable.h
@@ -1,5 +1,5 @@
 /***************************************************************************
- *   Copyright (C) 2015 by Mike Hewson                                     *
+ *   Copyright (C) 2016 by Mike Hewson                                     *
  *   hewsmike[AT]iinet.net.au                                              *
  *                                                                         *
  *   This file is part of Einstein@Home.                                   *
@@ -18,10 +18,59 @@
  *                                                                         *
  ***************************************************************************/
 
-#include "Bound.h"
+#ifndef BINDABLE_H_
+#define BINDABLE_H_
 
-Bound::Bound() {
-    }
+#include "ogl_utility.h"
 
-Bound::~Bound() {
-    }
+/**
+ * \addtogroup ogl_utility OGL_Utility
+ * @{
+ */
+
+/**
+ * \brief This interface declares public methods to deal with objects
+ *        that involve binding and unbinding to the OpenGL state machine.
+ *
+ * \author Mike Hewson\n
+ */
+
+class Bindable {
+    public:
+        /**
+         * \brief Constructor.
+         */
+        Bindable(void);
+
+        /**
+         * \brief Destructor.
+         */
+        virtual ~Bindable();
+
+        /**
+         * \brief Perform any binding to the OpenGL pipeline.
+         */
+        virtual void bind(void) = 0;
+
+        /**
+         * \brief Remove any binding to the OpenGL pipeline.
+         */
+        virtual void unbind(void) = 0;
+
+        /**
+         * \brief The binding state of the underlying OpenGL buffer
+         *        object to the OpenGL state machine. This MUST be a
+         *        dynamic inquiry.
+         *
+         * \return a boolean indicating binding state :
+         *          true - the object is bound
+         *          false - the object is not bound
+         */
+        virtual bool isBound(void) const = 0;
+    };
+
+/**
+ * @}
+ */
+
+#endif /*BINDABLE_H_*/
