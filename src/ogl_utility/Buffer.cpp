ea69750dafd8a198177b83985f2922f4b7a3d700
Mike Hewson <hewsmike@iinet.net.au>
2016-03-08 15:26:02 +1100

    Cleanup on Buffer

--- src/ogl_utility/Buffer.cpp
+++ src/ogl_utility/Buffer.cpp
@@ -45,40 +45,6 @@ Buffer::Buffer(const GLvoid* buffer_data, GLuint bytes) {
 Buffer::~Buffer() {
     }
 
-bool Buffer::acquire(void) {
-    // Only acquire if not already so.
-    if(this->isAcquired() == false) {
-        // Get a handle from the state machine.
-        GLuint handle;
-        this->acquire_ID(&handle);
-        glGenBuffers(1, &handle);
-        // Remember the value for later.
-        OGL_ID::set_ID(handle);
-
-        // Copy the data from client side to be stored in the state
-        // machine object.
-        loadBuffer();
-
-        // Record resources as acquired.
-        Buffer::setAcquisitionState(true);
-        }
-
-    /// TODO  Oddly we can't determine if the above worked via straight forward API calls.
-    return true;
-    }
-
-void Buffer::release(void) {
-    // Only release if already acquired.
-    if(this->isAcquired() == true) {
-        GLuint handle = this->ID();
-        this->release_ID(&handle);
-        // Reset to the null case.
-        OGL_ID::set_ID(OGL_ID::NO_ID);
-        // Mark as not acquired.
-        Buffer::setAcquisitionState(false);
-        }
-    }
-
 const GLvoid* Buffer::data(void) const {
     return m_data;
     }
