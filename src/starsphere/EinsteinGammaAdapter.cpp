a1bdb72e3f0e9c997e0ee37e8d137adfd221ad53
Mike Hewson <hewsmike@iinet.net.au>
2020-05-13 13:33:10 +1000

    New EinsteinGammaAdapter class

---
+++ src/starsphere/EinsteinGammaAdapter.cpp
@@ -0,0 +1,229 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Oliver Bock                                     *
+ *   oliver.bock[AT]aei.mpg.de                                             *
+ *                                                                         *
+ *   Copyright (C) 2020 by Mike Hewson                                     *
+ *   hewsmike[AT]iinet.net.au                                              *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "EinsteinGammaAdapter.h"
+
+#include <sstream>
+
+const string EinsteinGammaAdapter::SharedMemoryIdentifier = "EinsteinRadio";
+
+EinsteinGammaAdapter::EinsteinGammaAdapter(BOINCClientAdapter *boincClient) :
+   m_WUTemplatePowerSpectrum(POWERSPECTRUM_BINS, 0) {
+   this->boincClient = boincClient;
+   m_xmlReader = NULL;
+
+   m_WUSkyPosRightAscension = 0.0;
+   m_WUSkyPosDeclination = 0.0;
+   m_WUFractionDone = 0.0;
+   m_WUCPUTime = 0.0;
+   }
+
+EinsteinGammaAdapter::~EinsteinGammaAdapter() {
+   if(m_xmlReader) xmlFreeTextReader(m_xmlReader);
+      xmlCleanupParser();
+   }
+
+void EinsteinGammaAdapter::refresh() {
+   boincClient->refresh();
+   parseApplicationInformation();
+   }
+
+void EinsteinGammaAdapter::parseApplicationInformation() {
+   // get updated application information
+   string info = boincClient->applicationInformation();
+
+   // do we have any data?
+   if(info.length() > 0) {
+      int result = 0;
+
+      // prepare conversion stream
+      stringstream converter;
+      converter.precision(3);
+      converter.exceptions(ios_base::badbit | ios_base::failbit);
+
+      if(!m_xmlReader) {
+         // set up SAX style XML reader (create instance)
+         m_xmlReader = xmlReaderForMemory(info.c_str(),
+                                  info.length(),
+                                  "http://einstein.phys.uwm.edu",
+                                  "UTF-8",
+                                  0);
+         if(!m_xmlReader) {
+            cerr << "Error creating XML reader for shared memory data!" << endl;
+            return;
+            }
+         }
+      else {
+         // set up SAX style XML reader (reusing existing instance)
+         if(xmlReaderNewMemory(m_xmlReader,
+                          info.c_str(),
+                          info.length(),
+                          "http://einstein.phys.uwm.edu",
+                          "UTF-8",
+                          0)) {
+            cerr << "Error updating XML reader for shared memory data!" << endl;
+            return;
+            }
+         }
+
+      // parse XML fragment and process nodes
+      result = xmlTextReaderRead(m_xmlReader);
+      while (result == 1) {
+         processXmlNode(m_xmlReader, converter);
+            result = xmlTextReaderRead(m_xmlReader);
+         }
+
+      // convert radians to degrees
+      m_WUSkyPosRightAscension *= 180/PI;
+      m_WUSkyPosDeclination *= 180/PI;
+
+      // deserialize power spectrum data
+      if(m_WUTemplatePowerSpectrumString.length() == POWERSPECTRUM_BIN_BYTES) {
+         int spectrumBinValue;
+
+         // iterate over all bins
+         for(int i = 0, j = 0; i < POWERSPECTRUM_BIN_BYTES; i += 2, ++j) {
+            try {
+               converter.clear();
+               converter.str(m_WUTemplatePowerSpectrumString.substr(i, 2));
+               // convert hex bin value to integer
+               converter >> hex >> spectrumBinValue;
+               // store bin power value
+               m_WUTemplatePowerSpectrum.at(j) = (unsigned char) spectrumBinValue;
+               }
+            catch(ios_base::failure) {
+               cerr << "Error processing power spectrum shared memory data!" << endl;
+               break;
+               }
+            }
+          }
+      else {
+         cerr << "Invalid power spectrum shared memory data encountered!" << endl;
+         }
+      }
+   }
+
+void EinsteinGammaAdapter::processXmlNode(const xmlTextReaderPtr xmlReader,
+                                          stringstream& converter) {
+   // we only parse element nodes
+   if(xmlTextReaderNodeType(xmlReader) != XML_READER_TYPE_ELEMENT) return;
+
+   // buffers (will be deallocated automatically)
+   const xmlChar *nodeName = NULL, *nodeValue = NULL;
+
+   // get element node's name
+   nodeName = xmlTextReaderConstLocalName(xmlReader);
+
+   if (nodeName == NULL) {
+      cerr << "Error parsing XML node (invalid name)" << endl;
+      return;
+      }
+
+   // move to node's text content (child node) or return if unavailable
+   if(! (xmlTextReaderRead(m_xmlReader) && xmlTextReaderHasValue(xmlReader))) {
+      return;
+      }
+
+   // get text node's value
+   nodeValue = xmlTextReaderConstValue(xmlReader);
+
+   if (nodeValue == NULL) {
+      cerr << "Error parsing XML node (invalid value)" << endl;
+      return;
+      }
+
+   try {
+        // prepare converter stream
+        converter.clear();
+        converter.str("");
+        converter << nodeValue;
+
+      // assign node value to respective data member
+      if(xmlStrEqual(nodeName, BAD_CAST("skypos_rac"))) {
+         converter >> fixed >> m_WUSkyPosRightAscension;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("skypos_dec"))) {
+         converter >> fixed >> m_WUSkyPosDeclination;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("dispersion"))) {
+         converter >> fixed >> m_WUDispersionMeasure;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("orb_radius"))) {
+         converter >> fixed >> m_WUTemplateOrbitalRadius;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("orb_period"))) {
+         converter >> fixed >> m_WUTemplateOrbitalPeriod;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("orb_phase"))) {
+         converter >> fixed >> m_WUTemplateOrbitalPhase;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("power_spectrum"))) {
+         converter >> m_WUTemplatePowerSpectrumString;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("fraction_done"))) {
+         converter >> fixed >> m_WUFractionDone;
+         }
+      else if(xmlStrEqual(nodeName, BAD_CAST("cpu_time"))) {
+         converter >> fixed >> m_WUCPUTime;
+         }
+      }
+   catch(ios_base::failure) {
+      cerr << "Error converting XML reader node content!" << endl;
+      }
+   }
+
+double EinsteinGammaAdapter::wuSkyPosRightAscension() const {
+   return m_WUSkyPosRightAscension;
+   }
+
+double EinsteinGammaAdapter::wuSkyPosDeclination() const {
+   return m_WUSkyPosDeclination;
+   }
+
+double EinsteinGammaAdapter::wuDispersionMeasure() const {
+   return m_WUDispersionMeasure;
+   }
+
+double EinsteinGammaAdapter::wuTemplateOrbitalRadius() const {
+   return m_WUTemplateOrbitalRadius;
+   }
+
+double EinsteinGammaAdapter::wuTemplateOrbitalPeriod() const {
+   return m_WUTemplateOrbitalPeriod;
+   }
+
+double EinsteinGammaAdapter::wuTemplateOrbitalPhase() const {
+   return m_WUTemplateOrbitalPhase;
+   }
+
+const vector<unsigned char>* EinsteinGammaAdapter::wuTemplatePowerSpectrum() const {
+   return &m_WUTemplatePowerSpectrum;
+   }
+
+double EinsteinGammaAdapter::wuFractionDone() const {
+   return m_WUFractionDone;
+   }
+
+double EinsteinGammaAdapter::wuCPUTime() const {
+   return m_WUCPUTime;
+   }
---
+++ src/starsphere/EinsteinGammaAdapter.h
@@ -0,0 +1,212 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Oliver Bock                                     *
+ *   oliver.bock[AT]aei.mpg.de                                             *
+ *                                                                         *
+ *   Copyright (C) 2020 by Mike Hewson                                     *
+ *   hewsmike[AT]iinet.net.au                                              *
+ *                                                                         *
+ *   This file is part of Einstein@Home.                                   *
+ *                                                                         *
+ *   Einstein@Home is free software: you can redistribute it and/or modify *
+ *   it under the terms of the GNU General Public License as published     *
+ *   by the Free Software Foundation, version 2 of the License.            *
+ *                                                                         *
+ *   Einstein@Home is distributed in the hope that it will be useful,      *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the          *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with Einstein@Home. If not, see <http://www.gnu.org/licenses/>. *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef EINSTEINGAMMAADAPTER_H_
+#define EINSTEINGAMMAADAPTER_H_
+
+#include <string>
+#include <sstream>
+#include <iomanip>
+#include <vector>
+
+#include <libxml/xmlreader.h>
+
+#include "BOINCClientAdapter.h"
+
+using namespace std;
+
+#define PI 3.14159265
+#define POWERSPECTRUM_BINS 40
+#define POWERSPECTRUM_BIN_BYTES 80
+
+/**
+ * \addtogroup starsphere Starsphere
+ * @{
+ */
+
+/**
+ * \brief Adapter class which facilitates communications with the \b Einstein\@Home Gamma Pulsar application
+ *
+ * This adapter class can be used to query the \b Einstein\@Home Gamma Pulsar application
+ * for informational data about the current work unit like search details and progress.
+ *
+ * \author Oliver Bock\n
+ * Max-Planck-Institute for Gravitational Physics\n
+ * Hannover, Germany
+ */
+
+class EinsteinGammaAdapter {
+   public:
+      /**
+       * \brief Constructor
+       *
+       * \param boincClient Pointer to the parent BOINC client adapter instance
+       */
+      EinsteinGammaAdapter(BOINCClientAdapter* boincClient);
+
+      /// Destructor
+      virtual ~EinsteinRadioAdapter();
+
+      /**
+       * \brief Refreshes dynamic data (e.g. search information)
+       *
+       * You want to call this method periodically to refresh any volatile application information
+       *
+       * \see AbstractGraphicsEngine::refreshBOINCInformation
+       */
+      void refresh();
+
+      /**
+       * \brief Retrieves the right ascension of the currently searched sky position
+       *
+       * \return The right ascension (in degrees)
+       */
+      double wuSkyPosRightAscension() const;
+
+      /**
+       * \brief Retrieves the declination of the currently searched sky position
+       *
+       * \return The right ascension (in degrees)
+       */
+      double wuSkyPosDeclination() const;
+
+      /**
+       * \brief Retrieves the dispersion measure of the currently searched sky position
+       *
+       * \return The dispersion measure
+       */
+      double wuDispersionMeasure() const;
+
+      /**
+       * \brief Retrieves the projected orbital radius of the currently active template
+       *
+       * \return The projected orbital radius
+       */
+      double wuTemplateOrbitalRadius() const;
+
+      /**
+       * \brief Retrieves the orbital period of the currently active template
+       *
+       * \return The orbital period of the currently active template
+       */
+      double wuTemplateOrbitalPeriod() const;
+
+      /**
+       * \brief Retrieves the initial orbital phase of the currently active template
+       *
+       * \return The initial orbital phase of the currently active template
+       */
+      double wuTemplateOrbitalPhase() const;
+
+      /**
+       * \brief Retrieves the power spectrum of the currently active template
+       *
+       * \return The power spectrum of the currently active template
+       */
+      const vector<unsigned char>* wuTemplatePowerSpectrum() const;
+
+      /**
+       * \brief Retrieves the completion fraction of the currently active work unit
+       *
+       * \return The completion fraction (range 0-1)
+       */
+      double wuFractionDone() const;
+
+      /**
+       * \brief Retrieves the amount of CPU time consumed for the currently active work unit
+       * during the active session
+       *
+       * \return The accumulated CPU time consumed during this work unit session (in seconds)
+       */
+      double wuCPUTime() const;
+
+      /// The identifier of the Einstein\@Home Radio Pulsar science application's shared memory area
+      static const string SharedMemoryIdentifier;
+
+   private:
+      /**
+       * \brief Parses science application specific information into local attributes
+       *
+       * The information is usually transferred via a shared memory area
+       * which is handled by the parent generic BOINC client adapter.
+       *
+       * \see boincClient
+       */
+      void parseApplicationInformation();
+
+      /**
+       * \brief Processes single element nodes found by the XML parser
+       *
+       * This method can sort of resembles the callback method used in conjunction with SAX parsers.
+       * It's called to process single XML nodes (in our case element nodes only) encountered
+       * during XML document (tree) traversal.
+       *
+       * \param xmlReader The pointer to the current xmlReader instance
+       * \param converter Reference to an existing string stream for value/type conversion
+       *
+       * \see parseApplicationInformation()
+       */
+      void processXmlNode(const xmlTextReaderPtr xmlReader, stringstream& converter);
+
+      /// Pointer to the (parent) BOINC client adapter
+      BOINCClientAdapter *boincClient;
+
+      /// Pointer to the XML reader (SAX style)
+      xmlTextReaderPtr m_xmlReader;
+
+      /// Right ascension of the currently searched sky position (in degrees)
+      double m_WUSkyPosRightAscension;
+
+      /// Declination of the currently searched sky position (in degrees)
+      double m_WUSkyPosDeclination;
+
+      /// Dispersion measure of the currently searched sky position
+      double m_WUDispersionMeasure;
+
+      /// Projected orbital radius of the currently active template
+      double m_WUTemplateOrbitalRadius;
+
+      /// Orbital period of the currently active template
+      double m_WUTemplateOrbitalPeriod;
+
+      /// Initial orbital phase of the currently active template
+      double m_WUTemplateOrbitalPhase;
+
+      /// Power spectrum of the currently active template (string)
+      string m_WUTemplatePowerSpectrumString;
+
+      /// Power spectrum of the currently active template (values)
+      vector<unsigned char> m_WUTemplatePowerSpectrum;
+
+      /// The completion fraction of the active work unit
+      double m_WUFractionDone;
+
+      /// Amount of CPU time consumed for the work unit during the active session
+      double m_WUCPUTime;
+   };
+
+/**
+ * @}
+ */
+
+#endif /*EINSTEINGAMMAADAPTER_H_*/
